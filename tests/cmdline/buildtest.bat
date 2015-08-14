50dc19f1a239afbeb39767193b60cbd0fd8e33e6
Brian Lamb <brianlam@microsoft.com>
2015-08-14 09:47:30 -0700

    Add a simple set of command-line/batch driven automated tests.  test_list.txt contains all the tests to be run and should be modified to add further tests.

---
+++ tests/cmdline/buildtest.bat
@@ -0,0 +1,17 @@
+@echo off
+SET LIBS=objcrt.lib;Starboard.lib;Foundation.lib;CoreFoundation.lib
+SET TESTNAME=%1
+
+SHIFT
+SET  TESTFILES=%1
+SHIFT
+
+:parsefiles
+if "%~1" neq "" (
+  set TESTFILES=%TESTFILES%;%1
+  shift
+  goto :parsefiles
+)
+
+echo Building %TESTNAME%
+msbuild /verbosity:quiet /nologo cmdline.vcxproj.template /property:TestName="%TESTNAME%" /property:TestFilenames="%TESTFILES%" /property:LinkLibraries="%LIBS%"
---
+++ tests/cmdline/runall.bat
@@ -0,0 +1,40 @@
+@echo off
+echo.
+echo Running tests from test_list.txt
+echo.
+
+SET CURTEST=
+SET PASSED=TRUE
+SETLOCAL enabledelayedexpansion
+IF EXIST test_results.txt DEL test_results.txt
+
+FOR /F "tokens=1 eol=#" %%t IN (test_list.txt) DO (
+    SET CURTEST=%%t
+    echo |set /p=!CURTEST! 
+    echo. >> test_results.txt 
+    echo =========^> Running !CURTEST! >> test_results.txt 
+    IF NOT EXIST bin\!CURTEST!.exe goto error
+    bin\!CURTEST!.exe >> test_results.txt 2>&1
+    IF NOT !ERRORLEVEL!==0 (
+        SET RESULT=FAILED err=!ERRORLEVEL!
+        SET PASSED=FALSE
+    ) ELSE (
+        SET RESULT=PASSED
+    )
+    echo ^<========= !CURTEST! !RESULT!>> test_results.txt 
+    echo. >> test_results.txt 
+    
+    echo !RESULT!
+)
+IF "%PASSED%"=="TRUE" goto passed
+echo.
+echo One or more tests FAILED
+echo.
+goto done
+:passed
+echo.
+echo All tests PASSED
+echo.
+:done
+echo See test_results.txt for logs
+echo.
