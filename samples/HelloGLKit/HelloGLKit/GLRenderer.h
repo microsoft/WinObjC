5c6f936157ee6d662173a79a5893d01348045efe
Brian Keron <brianker@microsoft.com>
2015-08-31 14:51:50 -0700

    Added some basic math functions.

--- samples/HelloGLKit/HelloGLKit/GLRenderer.h
+++ samples/HelloGLKit/HelloGLKit/GLRenderer.h
@@ -4,6 +4,9 @@
 
 @interface GLRenderer : NSObject<GLKViewDelegate, GLKViewControllerDelegate>
 
+-(void)initGLData;
+-(void)cleanupGLData;
+
 -(void)glkViewController: (GLKViewController*)controller willPause:(BOOL)paused;
 -(void)glkViewControllerUpdate: (GLKViewController*)controller;
 -(void)glkView:(GLKView*)view drawInRect:(CGRect)rect;
