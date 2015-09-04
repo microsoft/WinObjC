cc286c87aa96afc116ac1aab1bd8de6d7b11cd4c
Brian Lamb <brianlam@microsoft.com>
2015-09-04 11:44:49 -0700

    Move definitions for M_* math constants into WOCStblib math.h

--- include/StarboardAdditionalDefines.h
+++ include/StarboardAdditionalDefines.h
@@ -19,17 +19,6 @@
 
 #define TARGET_IPHONE_SIMULATOR 1   //  Temporary for Cocos2D compat, for now
 
-#ifndef M_PI
- #define M_PI        3.14159265358979323846264338327950288   /* pi */
-#endif
-#ifndef M_PI_2
- #define M_PI_2      1.57079632679489661923132169163975144   /* pi/2 */
-#endif
-
-#ifndef M_PI_4
- #define M_PI_4      0.785398163397448309615660845819875721  /* pi/4 */
-#endif
-
 enum {
     noErr = 0
 };
---
+++ include/WOCStdLib/math.h
@@ -0,0 +1,59 @@
+//******************************************************************************
+//
+// Copyright (c) 2015 Microsoft Corporation. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
+
+#pragma once
+
+#include_next <math.h>
+
+#ifndef M_E
+ #define M_E        2.7182818284590452354  // e
+#endif
+#ifndef M_LOG2E
+ #define M_LOG2E    1.4426950408889634074	// log 2e
+#endif
+#ifndef M_LOG10E
+ #define M_LOG10E   0.43429448190325182765  // log 10e
+#endif
+#ifndef M_LN2
+ #define M_LN2      0.69314718055994530942  // log e2
+#endif
+#ifndef M_LN10
+ #define M_LN10	    2.30258509299404568402	// log e10
+#endif
+#ifndef M_PI
+ #define M_PI        3.14159265358979323846264338327950288   //  pi
+#endif
+#ifndef M_PI_2
+ #define M_PI_2      1.57079632679489661923132169163975144   // pi/2
+#endif
+#ifndef M_PI_4
+ #define M_PI_4      0.785398163397448309615660845819875721  // pi/4
+#endif
+#ifndef M_1_PI
+ #define M_1_PI	     0.31830988618379067154 // 1/pi
+#endif
+#ifndef M_2_PI
+ #define M_2_PI      0.63661977236758134308 // 2/pi
+#endif
+#ifndef M_2_SQRTPI
+ #define M_2_SQRTPI  1.12837916709551257390 // 2/sqrt(pi)
+#endif
+#ifndef M_SQRT2
+ #define M_SQRT2     1.41421356237309504880 // sqrt(2)
+#endif
+#ifndef M_SQRT1_2
+ #define M_SQRT1_2   0.70710678118654752440 // 1/sqrt(2)
+#endif
