6109f5a046c52deeab22fa168933d4fb5c795101
Brian Lamb <brianlam@microsoft.com>
2015-09-08 11:57:13 -0700

    Remove angle from deps now that we're using the latest NuGet package.

--- deps/3rdparty/angleproject/samples/Asteroids/src/engine/interactive.cc
+++
@@ -1,5 +0,0 @@
-#include "interactive.h"
-#include "teges.h"
-
-Interactive::Interactive(Teges* _engine) : TegesObject(_engine) { }
-
--- deps/3rdparty/angleproject/samples/RotatingCube/winrtcompiler.bat
+++
@@ -1,2 +0,0 @@
-..\..\src\winrtcompiler\bin\winrtcompiler.exe -o=shader_winrt.h -p=winrt -a=gProgram -v=shader.vert -f=shader.frag
-..\..\src\winrtcompiler\bin\winrtcompiler.exe -o=shader_wp8.h -p=wp8 -a=gProgram -v=shader.vert -f=shader.frag
