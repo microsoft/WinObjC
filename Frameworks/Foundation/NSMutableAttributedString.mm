a50b2d75498d6d5c0c839ab6a7290e54c6e3a522
Jingtao Huang <jihua@microsoft.com>
2015-11-16 13:53:25 -0800

    NS(Mutable)AttributedString initial implementation
    Implements the majority of the CF/NS(Mutable)AttributedString interfaces that are not part of UI Kit extensions.
    A few have been left as stubs due to relative complexity/this checkin becoming too large.
    
    Added 40~ unit tests for NS(Mutable)AttributedString.
    
    Implemented CFStringReplace() to help unblock some of the required functionality.
    
    Reviewed-by: Brian Bowman <bbowman>
    Reviewed-by: Nithish Mahalingam <nithishm>

--- Frameworks/Foundation/NSMutableAttributedString.mm
+++ Frameworks/Foundation/NSMutableAttributedString.mm
@@ -15,7 +15,114 @@
 //******************************************************************************
 
 #import <Foundation/NSMutableAttributedString.h>
+#import <CoreFoundation/CFAttributedString.h>
 
 @implementation NSMutableAttributedString
+
+/**
+ @Status Stub
+*/
+- (NSMutableString*)mutableString {
+    return (__bridge NSMutableString*)CFAttributedStringGetMutableString(reinterpret_cast<CFMutableAttributedStringRef>(self));
+}
+
+/**
+ @Status Interoperable
+*/
+- (void)addAttribute:(NSString*)name value:(id)value range:(NSRange)range {
+    CFAttributedStringSetAttribute(reinterpret_cast<CFMutableAttributedStringRef>(self),
+                                   CFRangeMake(range.location, range.length),
+                                   (__bridge CFStringRef)name,
+                                   value);
+}
+
+/**
+ @Status Interoperable
+*/
+- (void)addAttributes:(NSDictionary*)attributes range:(NSRange)range {
+    CFAttributedStringSetAttributes(reinterpret_cast<CFMutableAttributedStringRef>(self),
+                                    CFRangeMake(range.location, range.length),
+                                    (__bridge CFDictionaryRef)attributes,
+                                    false);
+}
+
+/**
+ @Status Interoperable
+*/
+- (void)appendAttributedString:(NSAttributedString*)attributedString {
+    [self replaceCharactersInRange:NSMakeRange([self length], 0) withAttributedString:attributedString];
+}
+
+/**
+ @Status Interoperable
+*/
+- (void)deleteCharactersInRange:(NSRange)range {
+    [self replaceCharactersInRange:range withString:@""];
+}
+
+/**
+ @Status Interoperable
+*/
+- (void)removeAttribute:(NSString*)name range:(NSRange)range {
+    CFAttributedStringRemoveAttribute(reinterpret_cast<CFMutableAttributedStringRef>(self),
+                                      CFRangeMake(range.location, range.length),
+                                      (__bridge CFStringRef)name);
+}
+
+/**
+ @Status Interoperable
+ */
+- (void)insertAttributedString:(NSAttributedString*)attributedString atIndex:(NSUInteger)index {
+    [self replaceCharactersInRange:NSMakeRange(index, 0) withAttributedString:attributedString];
+}
+
+/**
+ @Status Interoperable
+*/
+- (void)replaceCharactersInRange:(NSRange)range withString:(NSString*)string {
+    CFAttributedStringReplaceString(reinterpret_cast<CFMutableAttributedStringRef>(self),
+                                    CFRangeMake(range.location, range.length),
+                                    (__bridge CFStringRef)string);
+}
+
+/**
+ @Status Interoperable
+*/
+- (void)replaceCharactersInRange:(NSRange)range withAttributedString:(NSAttributedString*)attributedString {
+    CFAttributedStringReplaceAttributedString(reinterpret_cast<CFMutableAttributedStringRef>(self),
+                                              CFRangeMake(range.location, range.length),
+                                              (__bridge CFAttributedStringRef)attributedString);
+}
+
+/**
+ @Status Interoperable
+*/
+- (void)setAttributes:(NSDictionary*)attributes range:(NSRange)range {
+    CFAttributedStringSetAttributes(reinterpret_cast<CFMutableAttributedStringRef>(self),
+                                    CFRangeMake(range.location, range.length),
+                                    (__bridge CFDictionaryRef)attributes,
+                                    true);
+}
+
+/**
+ @Status Interoperable
+*/
+- (void)setAttributedString:(NSAttributedString*)attributedString {
+    [self replaceCharactersInRange:NSMakeRange(0, [self length]) withAttributedString:attributedString];
+}
+
+/**
+ @Status Interoperable
+*/
+- (void)beginEditing {
+    CFAttributedStringBeginEditing(reinterpret_cast<CFMutableAttributedStringRef>(self));
+}
+
+/**
+ @Status Interoperable
+*/
+- (void)endEditing {
+    CFAttributedStringEndEditing(reinterpret_cast<CFMutableAttributedStringRef>(self));
+}
 
 @end
--- include/CoreFoundation/CFAttributedString.h
+++ include/CoreFoundation/CFAttributedString.h
@@ -1,10 +1,19 @@
-/* Copyright (c) 2008-2009 Christopher J. W. Lloyd
+//******************************************************************************
+//
+// Copyright (c) 2015 Microsoft Corporation. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
 
-Permission is hereby granted,free of charge,to any person obtaining a copy of this software and associated documentation files (the "Software"),to deal in the Software without restriction,including without limitation the rights to use,copy,modify,merge,publish,distribute,sublicense,and/or sell copies of the Software,and to permit persons to whom the Software is furnished to do so,subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS",WITHOUT WARRANTY OF ANY KIND,EXPRESS OR IMPLIED,INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,DAMAGES OR OTHER LIABILITY,WHETHER IN AN ACTION OF CONTRACT,TORT OR OTHERWISE,ARISING FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 #import <CoreFoundation/CFBase.h>
 #import <CoreFoundation/CFString.h>
 #import <CoreFoundation/CFDictionary.h>
@@ -12,36 +21,61 @@ THE SOFTWARE IS PROVIDED "AS IS",WITHOUT WARRANTY OF ANY KIND,EXPRESS OR IMPLIED
 ENABLE_IMPLICIT_BRIDGING
 COREFOUNDATION_EXTERNC_BEGIN
 
-typedef struct __NSMutableAttributedString *CFAttributedStringRef;
-typedef struct __NSMutableAttributedString *CFMutableAttributedStringRef;
+typedef struct __CFMutableAttributedString* CFAttributedStringRef;
+typedef struct __CFMutableAttributedString* CFMutableAttributedStringRef;
 
-COREFOUNDATION_EXPORT CFTypeID              CFAttributedStringGetTypeID(void);
+COREFOUNDATION_EXPORT CFTypeID CFAttributedStringGetTypeID(void);
 
-COREFOUNDATION_EXPORT CFAttributedStringRef CFAttributedStringCreate(CFAllocatorRef allocator,CFStringRef string,CFDictionaryRef attributes);
-COREFOUNDATION_EXPORT CFAttributedStringRef CFAttributedStringCreateWithSubstring(CFAllocatorRef allocator,CFAttributedStringRef self,CFRange range);
+COREFOUNDATION_EXPORT CFAttributedStringRef _CFAttributedStringCreateEmpty(void);
 
-COREFOUNDATION_EXPORT CFAttributedStringRef CFAttributedStringCreateCopy(CFAllocatorRef allocator,CFAttributedStringRef self);
+COREFOUNDATION_EXPORT CFAttributedStringRef CFAttributedStringCreate(CFAllocatorRef allocator,
+                                                                     CFStringRef string,
+                                                                     CFDictionaryRef attributes);
+COREFOUNDATION_EXPORT CFAttributedStringRef CFAttributedStringCreateWithSubstring(CFAllocatorRef allocator,
+                                                                                  CFAttributedStringRef self,
+                                                                                  CFRange range);
 
-COREFOUNDATION_EXPORT CFIndex               CFAttributedStringGetLength(CFAttributedStringRef self);
-COREFOUNDATION_EXPORT CFStringRef           CFAttributedStringGetString(CFAttributedStringRef self);
+COREFOUNDATION_EXPORT CFAttributedStringRef CFAttributedStringCreateCopy(CFAllocatorRef allocator, CFAttributedStringRef self);
 
-COREFOUNDATION_EXPORT CFTypeRef             CFAttributedStringGetAttribute(CFAttributedStringRef self,CFIndex location,CFStringRef name,CFRange *effectiveRange);
-COREFOUNDATION_EXPORT CFTypeRef             CFAttributedStringGetAttributeAndLongestEffectiveRange(CFAttributedStringRef self,CFIndex location,CFStringRef name,CFRange range,CFRange *longestEffectiveRange);
-COREFOUNDATION_EXPORT CFDictionaryRef       CFAttributedStringGetAttributes(CFAttributedStringRef self,CFIndex location,CFRange *effectiveRange);
-COREFOUNDATION_EXPORT CFDictionaryRef       CFAttributedStringGetAttributesAndLongestEffectiveRange(CFAttributedStringRef self,CFIndex location,CFRange range,CFRange *longestEffectiveRange);
+COREFOUNDATION_EXPORT CFIndex CFAttributedStringGetLength(CFAttributedStringRef self);
+COREFOUNDATION_EXPORT CFStringRef CFAttributedStringGetString(CFAttributedStringRef self);
+
+COREFOUNDATION_EXPORT CFTypeRef CFAttributedStringGetAttribute(CFAttributedStringRef self,
+                                                               CFIndex location,
+                                                               CFStringRef name,
+                                                               CFRange* effectiveRange);
+COREFOUNDATION_EXPORT CFTypeRef CFAttributedStringGetAttributeAndLongestEffectiveRange(
+    CFAttributedStringRef self, CFIndex location, CFStringRef name, CFRange range, CFRange* longestEffectiveRange);
+COREFOUNDATION_EXPORT CFDictionaryRef CFAttributedStringGetAttributes(CFAttributedStringRef self,
+                                                                      CFIndex location,
+                                                                      CFRange* effectiveRange);
+COREFOUNDATION_EXPORT CFDictionaryRef CFAttributedStringGetAttributesAndLongestEffectiveRange(CFAttributedStringRef self,
+                                                                                              CFIndex location,
+                                                                                              CFRange range,
+                                                                                              CFRange* longestEffectiveRange);
 // mutable
 
-COREFOUNDATION_EXPORT CFMutableAttributedStringRef CFAttributedStringCreateMutable(CFAllocatorRef allocator,CFIndex maxLength);
+COREFOUNDATION_EXPORT CFMutableAttributedStringRef CFAttributedStringCreateMutable(CFAllocatorRef allocator, CFIndex maxLength);
 
-COREFOUNDATION_EXPORT CFMutableAttributedStringRef CFAttributedStringCreateMutableCopy(CFAllocatorRef allocator,CFIndex maxLength,CFAttributedStringRef self);
+COREFOUNDATION_EXPORT CFMutableAttributedStringRef CFAttributedStringCreateMutableCopy(CFAllocatorRef allocator,
+                                                                                       CFIndex maxLength,
+                                                                                       CFAttributedStringRef self);
 
 COREFOUNDATION_EXPORT CFMutableStringRef CFAttributedStringGetMutableString(CFMutableAttributedStringRef self);
 
-COREFOUNDATION_EXPORT void CFAttributedStringRemoveAttribute(CFMutableAttributedStringRef self,CFRange range,CFStringRef name);
-COREFOUNDATION_EXPORT void CFAttributedStringReplaceAttributedString(CFMutableAttributedStringRef self,CFRange range,CFAttributedStringRef replacement);
-COREFOUNDATION_EXPORT void CFAttributedStringReplaceString(CFMutableAttributedStringRef self,CFRange range,CFStringRef replacement);
-COREFOUNDATION_EXPORT void CFAttributedStringSetAttribute(CFMutableAttributedStringRef self,CFRange range,CFStringRef name,CFTypeRef value);
-COREFOUNDATION_EXPORT void CFAttributedStringSetAttributes(CFMutableAttributedStringRef self,CFRange range,CFDictionaryRef replacement,Boolean clearPreviousAttributes);
+COREFOUNDATION_EXPORT void CFAttributedStringRemoveAttribute(CFMutableAttributedStringRef self, CFRange range, CFStringRef name);
+COREFOUNDATION_EXPORT void CFAttributedStringReplaceAttributedString(CFMutableAttributedStringRef self,
+                                                                     CFRange range,
+                                                                     CFAttributedStringRef replacement);
+COREFOUNDATION_EXPORT void CFAttributedStringReplaceString(CFMutableAttributedStringRef self, CFRange range, CFStringRef replacement);
+COREFOUNDATION_EXPORT void CFAttributedStringSetAttribute(CFMutableAttributedStringRef self,
+                                                          CFRange range,
+                                                          CFStringRef name,
+                                                          CFTypeRef value);
+COREFOUNDATION_EXPORT void CFAttributedStringSetAttributes(CFMutableAttributedStringRef self,
+                                                           CFRange range,
+                                                           CFDictionaryRef replacement,
+                                                           Boolean clearPreviousAttributes);
 
 COREFOUNDATION_EXPORT void CFAttributedStringBeginEditing(CFMutableAttributedStringRef self);
 COREFOUNDATION_EXPORT void CFAttributedStringEndEditing(CFMutableAttributedStringRef self);
