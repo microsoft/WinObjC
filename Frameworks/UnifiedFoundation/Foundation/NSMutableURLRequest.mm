a2cf194912b4a20deafd978308f25c0c2d1693c4
Jeyaram Jeyaraj <jejeyara@microsoft.com>
2015-12-17 16:45:02 -0800

    Implemented the use of properties for NSMutableURLRequest. Cleaned up code and enabled the inheritance from NSURLRequest to be used. Added unit tests for NSMutableURLRequest.

--- Frameworks/UnifiedFoundation/Foundation/NSMutableURLRequest.mm
+++ Frameworks/UnifiedFoundation/Foundation/NSMutableURLRequest.mm
@@ -14,128 +14,30 @@
 //
 //******************************************************************************
 
-#include "Starboard.h"
-#include "Foundation/NSURLRequest.h"
-#include "Foundation/NSString.h"
-#include "Foundation/NSMutableDictionary.h"
-#include "Foundation/NSMutableURLRequest.h"
-#include "NSURLRequestInternal.h"
+#import <Starboard.h>
+#import <Foundation/NSString.h>
+#import <Foundation/NSMutableDictionary.h>
+#import <Foundation/NSMutableURLRequest.h>
+#import <NSURLRequestInternal.h>
 
 @implementation NSMutableURLRequest
-
 /**
  @Status Interoperable
 */
-- (void)addValue:(id)value forHTTPHeaderField:(id)headerName {
+- (void)addValue:(NSString*)value forHTTPHeaderField:(NSString*)headerName {
     id existing;
 
-    existing = [_headerFields objectForKey:headerName];
+    existing = [_allHTTPHeaderFields objectForKey:headerName];
     if (existing != nil) {
         value = [[existing stringByAppendingString:@","] stringByAppendingString:value];
     }
-
-    [_headerFields setObject:[value stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] forKey:headerName];
+    [_allHTTPHeaderFields setObject:[value stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] forKey:headerName];
 }
 
 /**
  @Status Interoperable
 */
-- (void)setValue:(id)value forHTTPHeaderField:(id)headerName {
-    [_headerFields setObject:[value stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] forKey:headerName];
-}
-
-/**
- @Status Interoperable
-*/
-- (void)setHTTPMethod:(id)method {
-    _method.attach([method copy]);
-}
-
-/**
- @Status Interoperable
-*/
-- (void)setAllHTTPHeaderFields:(id)fields {
-    id state = [fields keyEnumerator];
-    id key;
-
-    [_headerFields removeAllObjects];
-    while ((key = [state nextObject]) != nil) {
-        id value = [fields objectForKey:key];
-
-        if ([key isKindOfClass:[NSString class]] && [value isKindOfClass:[NSString class]]) {
-            [_headerFields setObject:[value stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] forKey:key];
-        }
-    }
-}
-
-/**
- @Status Interoperable
-*/
-- (void)setHTTPBody:(id)body {
-    _body.attach([body copy]);
-}
-
-/**
- @Status Interoperable
-*/
-- (void)setHTTPBodyStream:(id)stream {
-    _bodyStream = stream;
-}
-
-/**
- @Status Stub
-*/
-- (void)setCachePolicy:(NSURLRequestCachePolicy)policy {
-    UNIMPLEMENTED();
-    _cachePolicy = policy;
-}
-
-/**
- @Status Interoperable
-*/
-- (void)setHTTPShouldHandleCookies:(BOOL)shouldHandleCookies {
-    _shouldHandleCookies = shouldHandleCookies ? true : false;
-}
-
-/**
- @Status Interoperable
-*/
-- (void)setURL:(id)url {
-    _url = url;
-}
-
-/**
- @Status Stub
-*/
-- (void)setTimeoutInterval:(double)interval {
-    UNIMPLEMENTED();
-    _timeoutInterval = interval;
-}
-
-/**
- @Status Stub
-*/
-- (void)setHTTPShouldUsePipelining:(BOOL)shouldPipeline {
-    UNIMPLEMENTED();
-}
-
-- (id)copyWithZone:(NSZone*)zone {
-    NSMutableURLRequest* ret = [[NSMutableURLRequest alloc] initWithURL:(id)_url
-                                                            cachePolicy:NSURLRequestUseProtocolCachePolicy
-                                                        timeoutInterval:30.0]; /* [BUG: Make it copy all properties] */
-    ret->_headerFields = [_headerFields mutableCopy];
-    ret->_method.attach([_method copy]);
-    ret->_body.attach([_body copy]);
-    ret->_shouldHandleCookies = _shouldHandleCookies;
-    ret->_cachePolicy = _cachePolicy;
-    ret->_bodyStream = _bodyStream;
-    ret.mainDocumentURL = _mainDocumentURL;
-
-    return ret;
-}
-
-- (void)dealloc {
-    [_mainDocumentURL release];
-    [super dealloc];
+- (void)setValue:(NSString*)value forHTTPHeaderField:(NSString*)headerName {
+    [_allHTTPHeaderFields setObject:[value stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] forKey:headerName];
 }
 @end
