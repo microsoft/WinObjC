387352a7aea85054ee2b1d7a96c2c21273b0275c
Jordan Miner <jminer7@gmail.com>
2016-01-04 20:24:39 -0600

    Fix mistake in quaternion multiplication
    See https://en.wikipedia.org/wiki/Quaternion#Hamilton_product

--- include/GLKit/GLKMath.h
+++ include/GLKit/GLKMath.h
@@ -987,8 +987,8 @@ inline float GLKQuaternionDot(GLKQuaternion q1, GLKQuaternion q2) {
 inline GLKQuaternion GLKQuaternionMultiply(GLKQuaternion q1, GLKQuaternion q2) {
     GLKQuaternion res;
 
-    res.x = q1.w * q2.x + q1.x + q2.w + q1.y * q2.z - q1.z * q2.y;
-    res.y = q1.w * q2.y - q1.x * q2.z + q1.y * q2.w + q1.z + q2.x;
+    res.x = q1.w * q2.x + q1.x * q2.w + q1.y * q2.z - q1.z * q2.y;
+    res.y = q1.w * q2.y - q1.x * q2.z + q1.y * q2.w + q1.z * q2.x;
     res.z = q1.w * q2.z + q1.x * q2.y - q1.y * q2.x + q1.z * q2.w;
     res.w = q1.w * q2.w - q1.x * q2.x - q1.y * q2.y - q1.z * q2.z;
 
