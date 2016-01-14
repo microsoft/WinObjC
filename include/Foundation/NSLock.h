4898afd2b3eb40a9c152e5b2ffb2658ff2c29639
Raj Seshasankaran <rajsesh@microsoft.com>
2016-01-14 11:09:36 -0800

    Revert "VSO:6087744 - Complete implementation of NSLock using std and full unit tests"
    This reverts commit 513337c69bd249dbed5387952bf67ef66a357102.

--- include/Foundation/NSLock.h
+++ include/Foundation/NSLock.h
@@ -1,16 +1,10 @@
 /* Copyright (c) 2006-2007 Christopher J. W. Lloyd
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
-following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 #import <Foundation/NSObject.h>
 
@@ -19,22 +13,23 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 @protocol NSLocking
 
 /**
-@Status Interoperable
+ @Status Interoperable
 */
-- (void)lock;
+-(void)lock;
 
 /**
-@Status Interoperable
+ @Status Interoperable
 */
-- (void)unlock;
+-(void)unlock;
 @end
 
 FOUNDATION_EXPORT_CLASS
 @interface NSLock : NSObject <NSLocking>
-@property (copy) NSString* name;
+-(NSString *)name;
+-(void)setName:(NSString *)value;
 
-- (BOOL)tryLock;
-- (BOOL)lockBeforeDate:(NSDate*)value;
+-(BOOL)tryLock;
+-(BOOL)lockBeforeDate:(NSDate *)value;
 
 @end
 
@@ -44,10 +39,10 @@ FOUNDATION_EXPORT_CLASS
 - (void)signal;
 
 - (void)wait;
-- (BOOL)waitUntilDate:(NSDate*)limit;
+- (BOOL)waitUntilDate:(NSDate *)limit;
 
-- (NSString*)name;
-- (void)setName:(NSString*)newName;
+- (NSString *)name;
+- (void)setName:(NSString *)newName;
 @end
 
 #import <Foundation/NSConditionLock.h>
