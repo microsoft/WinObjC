microsoft/WinObjC

commit b315a27c40240352d98c38f054f9aed3514fdeb5

Basic security framewirk stubs
VSO 5873622

include/Security/SecTrust.h
@@ -13,5 +13,32 @@
 // THE SOFTWARE.
 //
 //******************************************************************************
+#pragma once
 
-// This header file is not yet implemented
+#import <Security/SecBase.h>
+#import <Security/SecKey.h>
+#import <Security/SecCertificate.h>
+#import <stdint.h>
+
+// This header file is not yet fully implemented
+
+typedef struct __SecTrust* SecTrustRef;
+
+typedef NS_ENUM(uint32_t, SecTrustResultType) {
+    kSecTrustResultInvalid = 0,
+    kSecTrustResultProceed,
+    kSecTrustResultConfirm,
+    kSecTrustResultDeny,
+    kSecTrustResultUnspecified,
+    kSecTrustResultRecoverableTrustFailure,
+    kSecTrustResultFatalTrustFailure,
+    kSecTrustResultOtherError
+};
+
+SECURITY_EXPORT SecKeyRef SecTrustCopyPublicKey(SecTrustRef trust);
+SECURITY_EXPORT OSStatus SecTrustCreateWithCertificates(CFTypeRef certificates, CFTypeRef policies, SecTrustRef* trust);
+SECURITY_EXPORT OSStatus SecTrustEvaluate(SecTrustRef trust, SecTrustResultType* result);
+SECURITY_EXPORT CFIndex SecTrustGetCertificateCount(SecTrustRef trust);
+SECURITY_EXPORT SecCertificateRef SecTrustGetCertificateAtIndex(SecTrustRef trust, CFIndex ix);
+SECURITY_EXPORT OSStatus SecTrustSetPolicies(SecTrustRef trust, CFTypeRef policies);
+SECURITY_EXPORT OSStatus SecTrustSetAnchorCertificatesOnly(SecTrustRef trust, Boolean anchorCertificatesOnly);

Frameworks/Security/SecCertificate.mm
@@ -13,8 +13,23 @@
 // THE SOFTWARE.
 //
 //******************************************************************************
-#pragma once
 
-// This header file is not yet fully implemented
+#import <Starboard.h>
+#import <Security/SecCertificate.h>
 
-typedef struct __SecKey* SecKeyRef;
+/**
+@Status Stub
+*/
+
+SecCertificateRef SecCertificateCreateWithData(CFAllocatorRef allocator, CFDataRef data) {
+    UNIMPLEMENTED();
+    return nullptr;
+}
+
+/**
+@Status Stub
+*/
+CFDataRef SecCertificateCopyData(SecCertificateRef certificate) {
+    UNIMPLEMENTED();
+    return nullptr;
+}

Frameworks/Security/SecPolicy.mm
@@ -13,8 +13,22 @@
 // THE SOFTWARE.
 //
 //******************************************************************************
-#pragma once
 
-// This header file is not yet fully implemented
+#import <Starboard.h>
+#import <Security/SecPolicy.h>
 
-typedef struct __SecKey* SecKeyRef;
+/**
+@Status Stub
+*/
+SecPolicyRef SecPolicyCreateBasicX509(void) {
+    UNIMPLEMENTED();
+    return nullptr;
+}
+
+/**
+@Status Stub
+*/
+SecPolicyRef SecPolicyCreateSSL(Boolean server, CFStringRef hostname) {
+    UNIMPLEMENTED();
+    return nullptr;
+}

Frameworks/Security/SecTrust.mm
@@ -1,0 +1,74 @@
+//******************************************************************************
+//
+// Copyright (c) 2015 Microsoft Corporation. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
+
+#import <Starboard.h>
+#import <Security/SecTrust.h>
+
+/**
+@Status Stub
+*/
+SecKeyRef SecTrustCopyPublicKey(SecTrustRef trust) {
+    UNIMPLEMENTED();
+    return nullptr;
+}
+
+/**
+@Status Stub
+*/
+OSStatus SecTrustCreateWithCertificates(CFTypeRef certificates, CFTypeRef policies, SecTrustRef* trust) {
+    UNIMPLEMENTED();
+    return errSecUnimplemented;
+}
+
+/**
+@Status Stub
+*/
+OSStatus SecTrustEvaluate(SecTrustRef trust, SecTrustResultType* result) {
+    UNIMPLEMENTED();
+    return errSecUnimplemented;
+}
+
+/**
+@Status Stub
+*/
+CFIndex SecTrustGetCertificateCount(SecTrustRef trust) {
+    UNIMPLEMENTED();
+    return -1;
+}
+
+/**
+@Status Stub
+*/
+SecCertificateRef SecTrustGetCertificateAtIndex(SecTrustRef trust, CFIndex ix) {
+    UNIMPLEMENTED();
+    return nullptr;
+}
+
+/**
+@Status Stub
+*/
+OSStatus SecTrustSetPolicies(SecTrustRef trust, CFTypeRef policies) {
+    UNIMPLEMENTED();
+    return errSecUnimplemented;
+}
+
+/**
+@Status Stub
+*/
+OSStatus SecTrustSetAnchorCertificatesOnly(SecTrustRef trust, Boolean anchorCertificatesOnly) {
+    UNIMPLEMENTED();
+    return errSecUnimplemented;
+}

build/Security/lib/SecurityLib.vcxproj
@@ -30,6 +30,9 @@
     <ClangCompile Include="$(MSBuildThisFileDirectory)..\..\..\Frameworks\Security\GenericPasswordItemHandler.mm" />
     <ClangCompile Include="$(MSBuildThisFileDirectory)..\..\..\Frameworks\Security\SecItem.mm" />
     <ClangCompile Include="$(MSBuildThisFileDirectory)..\..\..\Frameworks\Security\SecRandom.mm" />
+    <ClangCompile Include="$(MSBuildThisFileDirectory)..\..\..\Frameworks\Security\SecPolicy.mm" />
+    <ClangCompile Include="$(MSBuildThisFileDirectory)..\..\..\Frameworks\Security\SecTrust.mm" />
+    <ClangCompile Include="$(MSBuildThisFileDirectory)..\..\..\Frameworks\Security\SecCertificate.mm" />
   </ItemGroup>
   <PropertyGroup Label="Globals">
     <ProjectGuid>{BE591B6E-DC7E-4669-9F41-4898C271C417}</ProjectGuid>

include/Foundation/NSSet.h
@@ -1,10 +1,16 @@
 /* Copyright (c) 2006-2007 Christopher J. W. Lloyd
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 #ifndef _NSSET_H_
 #define _NSSET_H_
@@ -21,49 +27,50 @@ struct NSSetTable {
 
 FOUNDATION_EXPORT_CLASS
 @interface NSSet : NSObject <NSCoding, NSCopying, NSMutableCopying, NSFastEnumeration> {
-    @public
+@public
     struct NSSetTable _table;
 }
 
-- initWithObjects:(id *)objects count:(NSUInteger)count;
-- initWithArray:(NSArray *)array;
-- initWithSet:(NSSet *)set;
-- initWithSet:(NSSet *)set copyItems:(BOOL)copyItems;
-- initWithObjects:first,...;
+- initWithObjects:(id*)objects count:(NSUInteger)count;
+- initWithArray:(NSArray*)array;
+- initWithSet:(NSSet*)set;
+- initWithSet:(NSSet*)set copyItems:(BOOL)copyItems;
+- initWithObjects:first, ...;
 
 + set;
-+ setWithArray:(NSArray *)array;
-+ setWithSet:(NSSet *)set;
++ setWithArray:(NSArray*)array;
++ setWithSet:(NSSet*)set;
 + setWithObject:object;
-+ setWithObjects:first,...;
-+ setWithObjects:(id *)objects count:(NSUInteger)count;
++ setWithObjects:first, ...;
++ setWithObjects:(id*)objects count:(NSUInteger)count;
 
-- (NSSet *)setByAddingObject:object;
-- (NSSet *)setByAddingObjectsFromSet:(NSSet *)other;
-- (NSSet *)setByAddingObjectsFromArray:(NSArray *)array;
+- (NSSet*)setByAddingObject:object;
+- (NSSet*)setByAddingObjectsFromSet:(NSSet*)other;
+- (NSSet*)setByAddingObjectsFromArray:(NSArray*)array;
 
+- (NSArray*)sortedArrayUsingDescriptors:(NSArray*)descriptors;
 - member:object;
 - (NSUInteger)count;
-- (NSEnumerator *)objectEnumerator;
+- (NSEnumerator*)objectEnumerator;
 
-- (BOOL)isEqualToSet:(NSSet *)set;
+- (BOOL)isEqualToSet:(NSSet*)set;
 
-- (NSArray *)allObjects;
+- (NSArray*)allObjects;
 
 - (BOOL)containsObject:(id)object;
-- (BOOL)isSubsetOfSet:(NSSet *)set;
+- (BOOL)isSubsetOfSet:(NSSet*)set;
 
-- (BOOL)intersectsSet:(NSSet *)set;
+- (BOOL)intersectsSet:(NSSet*)set;
 
 - (void)makeObjectsPerformSelector:(SEL)selector;
 - (void)makeObjectsPerformSelector:(SEL)selector withObject:argument;
 
 - anyObject;
 
-- (NSString *)descriptionWithLocale:(NSDictionary *)locale;
-- (NSSet *)objectsPassingTest:(BOOL (^)(id obj, BOOL *stop))predicate;
-- (void)enumerateObjectsUsingBlock:(void (^)(id obj, BOOL *stop))block;
-- (void)enumerateObjectsWithOptions:(NSEnumerationOptions)opts usingBlock:(void (^)(id obj, BOOL *stop))block;
+- (NSString*)descriptionWithLocale:(NSDictionary*)locale;
+- (NSSet*)objectsPassingTest:(BOOL (^)(id obj, BOOL* stop))predicate;
+- (void)enumerateObjectsUsingBlock:(void (^)(id obj, BOOL* stop))block;
+- (void)enumerateObjectsWithOptions:(NSEnumerationOptions)opts usingBlock:(void (^)(id obj, BOOL* stop))block;
 
 @end
 

include/Security/SecBase.h
@@ -29,6 +29,7 @@
 #define SECURITY_EXPORT_CLASS SECURITY_IMPEXP
 #endif
 #endif
+
 enum {
     errSecSuccess = 0, /* No error. */
     errSecUnimplemented = -4,

include/Security/SecCertificate.h
@@ -13,5 +13,13 @@
 // THE SOFTWARE.
 //
 //******************************************************************************
+#pragma once
 
-// This header file is not yet implemented
+#import <Security/SecBase.h>
+
+// This header file is not yet fully implemented
+
+typedef struct __SecCertificate* SecCertificateRef;
+
+SECURITY_EXPORT SecCertificateRef SecCertificateCreateWithData(CFAllocatorRef allocator, CFDataRef data);
+SECURITY_EXPORT CFDataRef SecCertificateCopyData(SecCertificateRef certificate);

commit dc7f2a3ef757ad95a5eab5630cec0d7534c29a36

BUILD: Fix build break introduced by commit b315a27c40240352d98c38f054f9aed3514fdeb5

include/Security/SecTrust.h
@@ -24,7 +24,8 @@
 
 typedef struct __SecTrust* SecTrustRef;
 
-typedef NS_ENUM(uint32_t, SecTrustResultType) {
+typedef uint32_t SecTrustResultType;
+enum {
     kSecTrustResultInvalid = 0,
     kSecTrustResultProceed,
     kSecTrustResultConfirm,