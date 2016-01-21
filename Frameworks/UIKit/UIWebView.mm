3a52d9c163a693911a94be8fb2f8466b00b49add
Dave Lamb <davelamb@microsoft.com>
2016-01-21 15:18:07 -0800

    Enable setting background colors on UIWebview, including transparency.

--- Frameworks/UIKit/UIWebView.mm
+++ Frameworks/UIKit/UIWebView.mm
@@ -235,6 +235,27 @@ static void initWebKit(UIWebView* self) {
     return _request;
 }
 
+/**
+ @Status Interoperable
+*/
+- (void)setBackgroundColor:(UIColor*)color {
+    CGFloat r = 0;
+    CGFloat g = 0;
+    CGFloat b = 0;
+    CGFloat a = 0;
+
+    [super setBackgroundColor:color];
+
+    [color getRed:&r green:&g blue:&b alpha:&a];
+    
+    // XAML WebView transparency is not used unless it's set to the transparent system color.
+    if (a != 1.0f) {
+        [_xamlWebControl setDefaultBackgroundColor:[WUColors transparent]];
+    } else {
+        [_xamlWebControl setDefaultBackgroundColor:[WUColorHelper fromArgb:255 r:(unsigned char)(r * 255.0) g:(unsigned char)(g * 255.0) b:(unsigned char)(b * 255.0)]];
+    }
+}
+
 - (void)dealloc {
     _delayLoadURL = nil;
     [_xamlWebControl removeLoadCompletedEvent:_xamlLoadCompletedEventCookie];
