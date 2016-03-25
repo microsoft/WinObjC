a522be7ad1dcc601d28ed3d8d229c317746ac025
Ben Viglietta <benvi@microsoft.com>
2016-03-25 09:19:34 -0700

    Eliminate most uses of __declspec(dllexport) in favor of .def files

--- Frameworks/include/MurmurHash3.h
+++ Frameworks/include/MurmurHash3.h
@@ -27,15 +27,14 @@
 // Microsoft Visual Studio
 
 #include <stdint.h>
-#include <StarboardExport.h>
 
 //-----------------------------------------------------------------------------
 
-SB_EXPORT void MurmurHash3_x86_32(const void* key, int len, uint32_t seed, void* out);
+void MurmurHash3_x86_32(const void* key, int len, uint32_t seed, void* out);
 
-SB_EXPORT void MurmurHash3_x86_128(const void* key, int len, uint32_t seed, void* out);
+void MurmurHash3_x86_128(const void* key, int len, uint32_t seed, void* out);
 
-SB_EXPORT void MurmurHash3_x64_128(const void* key, int len, uint32_t seed, void* out);
+void MurmurHash3_x64_128(const void* key, int len, uint32_t seed, void* out);
 
 //-----------------------------------------------------------------------------
 
