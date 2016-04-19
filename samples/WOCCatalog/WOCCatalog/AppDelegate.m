902c9931bb5ea2ed34a383e7965e03d77f70888d
amit agarwal <amaga@microsoft.com>
2016-04-19 11:07:04 -0700

    Fix for menu items getting hidden by navigation bar on windows phone

--- samples/WOCCatalog/WOCCatalog/AppDelegate.m
+++ samples/WOCCatalog/WOCCatalog/AppDelegate.m
@@ -16,6 +16,10 @@
 
 #import "AppDelegate.h"
 #import "MainViewController.h"
+#ifdef WINOBJC
+#import "UWP/WindowsUIViewManagement.h"
+#import "UWP/WindowsFoundationMetadata.h"
+#endif
 
 #ifdef WINOBJC
 // Tell the WinObjC runtime how large to render the application
@@ -46,8 +50,28 @@
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
 
+#ifdef WINOBJC
+    WFMPlatform platform = [self detectPlatform];
+    if (platform == WFMPlatformWindowsPhone) {
+        // On windows phone the navigation bar hides a part of screen at the bottom, so we try to enter full screen mode on a windows phone.
+        [[WUVApplicationView getForCurrentView] tryEnterFullScreenMode];
+    }
+#endif
     return YES;
 }
+
+#ifdef WINOBJC
+- (WFMPlatform)detectPlatform {
+    // Using ApiInformation class we ask if Windows.Phone.PhoneContract is present to detect if the device is a windows phone, with the
+    // current introduced version number 1.
+    BOOL isWindowsPhone = [WFMApiInformation isApiContractPresentByMajor:@"Windows.Phone.PhoneContract" majorVersion:1];
+    if (isWindowsPhone) {
+        return WFMPlatformWindowsPhone;
+    } else {
+        return WFMPlatformWindows;
+    }
+}
+#endif
 
 - (void)applicationWillResignActive:(UIApplication*)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions
