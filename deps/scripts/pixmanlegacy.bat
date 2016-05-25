a5cb406bccf7846b33d049e456e0931015e3c4c6
Meredith Monticello <memontic@microsoft.com>
2016-05-25 16:35:01 -0700

    Updated library scripts and VS solutions so that they build Modified vcxproj's to copy headers and libraries to correct paths Added script, createlibs, to build all of them at once

--- deps/scripts/pixmanlegacy.bat
+++ deps/scripts/pixmanlegacy.bat
@@ -7,9 +7,6 @@ if "%1" == "/?" goto Usage
 set BUILD.ARM=N
 set BUILD.x86=N
 set BUILD.x64=N
-set BUILD.win10=N
-set BUILD.win8.1=N
-set BUILD.phone8.1=N
 
 :: Iterate through arguments and set the right configuration
 for %%a in (%*) do (
@@ -19,12 +16,6 @@ for %%a in (%*) do (
         set BUILD.x86=Y
     ) else if /I "%%a"=="x64" (
         set BUILD.x64=Y
-    ) else if /I "%%a"=="win10" (
-        set BUILD.win10=Y
-    ) else if /I "%%a"=="win8.1" (
-        set BUILD.win8.1=Y
-    ) else if /I "%%a"=="phone8.1" (
-        set BUILD.phone8.1=Y
     ) else (
         goto Usage
     )
@@ -41,25 +32,15 @@ if %BUILD.ARM%==N (
     )
 )
 
-:: Set build all platform if none are specified
-if %BUILD.win10%==N (
-    if %BUILD.win8.1%==N (
-        if %BUILD.phone8.1%==N (
-            set BUILD.win10=Y
-            set BUILD.win8.1=Y
-            set BUILD.phone8.1=Y
-        )
-    )
-)
-
 pushd ..\3rdparty\pixmanlegacy
 
 :: Check for required tools
-if not defined MSBUILD_BIN goto :MissingMSBuild
-if not exist %MSBUILD_BIN% goto :MissingMSBuild
+if not defined MSBUILD_PATH goto :MissingMSBuild
+if not exist %MSBUILD_PATH% goto :MissingMSBuild
 
 :: Initialize common path
 set BUILD.DEPS.PATH=%~dp0..
+set MSBUILD_BIN="%MSBUILD_PATH%"
 
 :: Copy all header files to include directory
 echo:
@@ -69,9 +50,6 @@ xcopy /y /i pixman\pixman.h %BUILD.DEPS.PATH%\prebuilt\include\
 xcopy /y /i pixman\pixman-version.h %BUILD.DEPS.PATH%\prebuilt\include\
 
 :: Build and deploy Windows 10 library
-:Win10
-if %BUILD.win10%==N goto Win8.1
-
 :Win10x86
 if %BUILD.x86%==N goto Win10x64
 echo:
@@ -80,7 +58,7 @@ echo:
 
 %MSBUILD_BIN% Win10\pixmanlegacy.Windows10\pixmanlegacy.Windows10.vcxproj /p:Configuration=Release;Platform=x86
 
-xcopy /y /i Win10\pixmanlegacy.Windows10\Release\pixmanlegacy.Windows10\pixman.lib "%BUILD.DEPS.PATH%\prebuilt\Windows Universal\x86"
+xcopy /y /i Win10\pixmanlegacy.Windows10\Release\pixmanlegacy.Windows10\pixman.lib "%BUILD.DEPS.PATH%\prebuilt\Universal Windows\x86"
 
 :Win10x64
 if %BUILD.x64%==N goto Win10ARM
@@ -90,76 +68,18 @@ echo:
 
 %MSBUILD_BIN% Win10\pixmanlegacy.Windows10\pixmanlegacy.Windows10.vcxproj /p:Configuration=Release;Platform=x64
 
-xcopy /y /i Win10\pixmanlegacy.Windows10\x64\Release\pixmanlegacy.Windows10\pixman.lib "%BUILD.DEPS.PATH%\prebuilt\Windows Universal\x64"
+xcopy /y /i Win10\pixmanlegacy.Windows10\x64\Release\pixmanlegacy.Windows10\pixman.lib "%BUILD.DEPS.PATH%\prebuilt\Universal Windows\x64"
 
 
 :Win10ARM
-if %BUILD.ARM%==N goto Win8.1
+if %BUILD.ARM%==N goto Cleanup
 echo:
 echo Build pixman for Windows 10 apps ARM...
 echo:
 
 %MSBUILD_BIN% Win10\pixmanlegacy.Windows10\pixmanlegacy.Windows10.vcxproj /p:Configuration=Release;Platform=ARM
 
-xcopy /y /i Win10\pixmanlegacy.Windows10\ARM\Release\pixmanlegacy.Windows10\pixman.lib "%BUILD.DEPS.PATH%\prebuilt\Windows Universal\ARM"
-
-:: Build and deploy Windows 8.1 library
-:Win8.1
-if %BUILD.win8.1%==N goto Phone8.1
-
-:Win8.1x86
-if %BUILD.x86%==N goto Win8.1x64
-echo:
-echo Build pixman for Windows 8.1 apps x86...
-echo:
-
-%MSBUILD_BIN% Win8.1\pixmanlegacy.Windows\pixmanlegacy.Windows.vcxproj /p:Configuration=Release;Platform=x86
-
-xcopy /y /i Win8.1\pixmanlegacy.Windows\Release\pixmanlegacy.Windows\pixman.lib "%BUILD.DEPS.PATH%\prebuilt\Windows 8.1\x86"
-
-:Win8.1x64
-if %BUILD.x64%==N goto Win8.1ARM
-echo:
-echo Build pixman for Windows 8.1 apps x64...
-echo:
-
-%MSBUILD_BIN% Win8.1\pixmanlegacy.Windows\pixmanlegacy.Windows.vcxproj /p:Configuration=Release;Platform=x64
-
-xcopy /y /i Win8.1\pixmanlegacy.Windows\x64\Release\pixmanlegacy.Windows\pixman.lib "%BUILD.DEPS.PATH%\prebuilt\Windows 8.1\x64"
-
-:Win8.1ARM
-if %BUILD.ARM%==N goto Phone8.1
-echo:
-echo Build pixman for Windows 8.1 apps ARM...
-echo:
-
-%MSBUILD_BIN% Win8.1\pixmanlegacy.Windows\pixmanlegacy.Windows.vcxproj /p:Configuration=Release;Platform=ARM
-
-xcopy /y /i Win8.1\pixmanlegacy.Windows\ARM\Release\pixmanlegacy.Windows\pixman.lib "%BUILD.DEPS.PATH%\prebuilt\Windows 8.1\ARM"
-
-:: Build and deploy Windows Phone 8.1 library
-:Phone8.1
-if %BUILD.phone8.1%==N goto Cleanup
-
-:Phone8.1ARM
-if %BUILD.ARM%==N goto Phone8.1x86
-echo:
-echo Build pixman for Windows Phone 8.1 apps ARM...
-echo:
-
-%MSBUILD_BIN% Win8.1\pixmanlegacy.WindowsPhone\pixmanlegacy.WindowsPhone.vcxproj /p:Configuration=Release;Platform=ARM
-
-xcopy /y /i Win8.1\pixmanlegacy.WindowsPhone\ARM\Release\pixmanlegacy.WindowsPhone\pixman.lib "%BUILD.DEPS.PATH%\prebuilt\Windows Phone 8.1\ARM"
-
-:Phone8.1x86
-if %BUILD.x86%==N goto Cleanup
-echo:
-echo Build pixman for Windows Phone 8.1 apps x86...
-echo:
-
-%MSBUILD_BIN% Win8.1\pixmanlegacy.WindowsPhone\pixmanlegacy.WindowsPhone.vcxproj /p:Configuration=Release;Platform=x86
-
-xcopy /y /i Win8.1\pixmanlegacy.WindowsPhone\Release\pixmanlegacy.WindowsPhone\pixman.lib "%BUILD.DEPS.PATH%\prebuilt\Windows Phone 8.1\x86"
+xcopy /y /i Win10\pixmanlegacy.Windows10\ARM\Release\pixmanlegacy.Windows10\pixman.lib "%BUILD.DEPS.PATH%\prebuilt\Universal Windows\ARM"
 
 goto Cleanup
 
@@ -168,18 +88,16 @@ goto Cleanup
 echo The correct usage is:
 echo     %0 [option]
 echo:
-echo where [option] is: ARM ^| x86 ^| x64 ^| win10 ^| win8.1 ^| phone8.1
+echo where [option] is: ARM ^| x86 ^| x64
 echo:
 echo For example:
-echo     %0 win10
-echo     %0 phone8.1 ARM
 echo     %0 ARM x86
 goto :eof
 
 :MissingMSBuild
 echo MSBuild.exe is needed. Install and provide the executable path in MSBUILD_BIN environment variable. E.g.
 echo:
-echo     set MSBUILD_BIN="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+echo     set MSBUILD_BIN=C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe
 goto Cleanup
 
 :Cleanup
