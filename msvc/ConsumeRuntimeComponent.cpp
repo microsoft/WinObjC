13a8b398b3201ee99e7bd1faac019a8fb4c3ed40
Brian Lamb <brianlam@microsoft.com>
2016-07-11 13:43:33 -0700

    Fix ARM build breaks - Use only a single/alternatename: flag depending on platform

--- msvc/ConsumeRuntimeComponent.cpp
+++ msvc/ConsumeRuntimeComponent.cpp
@@ -23,27 +23,20 @@
 #pragma comment(linker, "/INCLUDE:__refMTAThread")
 #endif
 
-
 extern "C"
 {
-    int __cdecl EbrDefaultXamlMain()
+    int __cdecl WOCMain(::Platform::Array<::Platform::String^>^ args)
     {
         int main(int argc, char *argv[]);
 
         return main(0, NULL);
     }
-    #pragma comment(linker, "/alternatename:?main@@YAHP$01$AAV?$Array@P$AAVString@Platform@@$00@Platform@@@Z=_EbrDefaultXamlMain")
 }
 
 #ifdef _M_ARM
-int __cdecl WOCMain(::Platform::Array<::Platform::String^>^ args)
-{
-    EbrDefaultXamlMain();
-
-    return 0;
-}
-
-#pragma comment(linker, "/alternatename:?main@@YAHP$01$AAV?$Array@P$AAVString@Platform@@$00@Platform@@@Z=?WOCMain@@YAHP$01$AAV?$Array@P$AAVString@Platform@@$00@Platform@@@Z")
+ #pragma comment(linker, "/alternatename:?main@@YAHP$01$AAV?$Array@P$AAVString@Platform@@$00@Platform@@@Z=WOCMain")
+#else
+ #pragma comment(linker, "/alternatename:?main@@YAHP$01$AAV?$Array@P$AAVString@Platform@@$00@Platform@@@Z=_WOCMain")
 #endif
 #else // _WOC_APP
 // We need to export something to ensure we link a viable PE
