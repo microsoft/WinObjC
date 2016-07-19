f88b14cee8f86e14967d7d43315f8f10872680d3
Dustin L. Howett <duhowett@microsoft.com>
2016-07-19 16:15:32 -0700

    Replace our manual strings format parser with CoreFoundation's.

--- tests/unittests/Foundation/NSStringTests.m
+++ tests/unittests/Foundation/NSStringTests.m
@@ -592,3 +592,25 @@ TEST(NSString, Exceptions) {
     NSString* immutableFormattedString = [NSString stringWithFormat:@"Hello %p", buffer];
     EXPECT_ANY_THROW([(NSMutableString*)immutableFormattedString replaceCharactersInRange:range withString:@"boom"]);
 }
+
+class StringsFormatPropertyList : public ::testing::TestWithParam<const wchar_t*> {};
+
+TEST_P(StringsFormatPropertyList, CanDeserialize) {
+    const wchar_t* data = GetParam();
+    NSString* string = [NSString stringWithCharacters:(const unichar*)data length:wcslen(data)];
+
+    ASSERT_OBJCNE(nil, string);
+
+    NSDictionary* propertyList = [string propertyListFromStringsFileFormat];
+
+    ASSERT_OBJCNE(nil, propertyList);
+
+    ASSERT_OBJCEQ(@"value1", propertyList[@"key1"]);
+    ASSERT_OBJCEQ(@"value2", propertyList[@"key2"]);
+}
+
+INSTANTIATE_TEST_CASE_P(NSString,
+                        StringsFormatPropertyList,
+                        ::testing::Values(L"\uFEFFkey1=value1;\n\"key2\"=\"value2\";", // BOM
+                                          L"key1=value1;\n\"key2\"=\"value2\";" // No BOM
+                                          ));
