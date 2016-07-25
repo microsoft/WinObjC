634f2e89758f599855ed5775d738b4fa4ac15503
Dustin L. Howett <duhowett@microsoft.com>
2016-07-25 16:07:00 -0700

    ___pin_objc_init has to be selectany to work.

--- msvc/objcrt/objc_2stage_init.cpp
+++ msvc/objcrt/objc_2stage_init.cpp
@@ -92,7 +92,7 @@ extern "C" const InitFunc __declspec(selectany) _SEGALLOC(".CRT$XCLB") ___objc_m
 // we would be resolving classes that have not been loaded.
 extern "C" const InitFunc __declspec(selectany) _SEGALLOC(".CRT$XCV") ___objc_legacy_post_XCU = &___objc_legacy_resolve_all;
 
-extern "C" const void* ___pin_objc_init = 0x0;
+extern "C" __declspec(selectany) const void* ___pin_objc_init = 0x0;
 
 // Force inclusion of this TU.
 #if defined(_M_IX86)
