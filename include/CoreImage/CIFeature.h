b0390a159b2af67e9ec397306711943e00945092
Raj Seshasankaran <rajsesh@microsoft.com>
2016-08-22 17:00:40 -0700

    Add CGBase.h and include CFBase.h to get basic datatypes. Fix #458. (#664)
    * Add CGBase.h and move CoreGraphics datatypes to it, fixing up headers in the process. Fix #458.
    
    * code review feedback

--- include/CoreImage/CIFeature.h
+++ include/CoreImage/CIFeature.h
@@ -1,6 +1,6 @@
 //******************************************************************************
 //
-// Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 //
 // This code is licensed under the MIT License (MIT).
 //
@@ -16,6 +16,7 @@
 #pragma once
 
 #import <CoreImage/CoreImageExport.h>
+#import <CoreGraphics/CGBase.h>
 #import <Foundation/NSObject.h>
 
 @class NSString;
