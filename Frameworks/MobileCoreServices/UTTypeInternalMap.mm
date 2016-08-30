c7c24c1806ae88308552015d7dc6314089778e79
Meredith Monticello <memontic@microsoft.com>
2016-08-30 11:17:10 -0700

    Added 'cer' type to UTIs Removed '.' prefix for all UTI extensions

--- Frameworks/MobileCoreServices/UTTypeInternalMap.mm
+++ Frameworks/MobileCoreServices/UTTypeInternalMap.mm
@@ -31,7 +31,7 @@ static CFMutableDictionaryRef s_systemUTIMap;
 static CFMutableDictionaryRef s_fileExtensionToSystemUTIMap;
 static CFMutableDictionaryRef s_mimeTypeToSystemUTIMap;
 
-typedef std::array<SystemUTIMapEntry, 38> SystemUTIMapEntryArray;
+typedef std::array<SystemUTIMapEntry, 39> SystemUTIMapEntryArray;
 
 /**
  * Returns a static system UTI map.
@@ -46,193 +46,198 @@ SystemUTIMapEntryArray& _UTGetSytemUTIMapping() {
         // }
         {
             kUTTypePlainText,
-            {CFSTR(".txt")},
+            {CFSTR("txt")},
             {CFSTR("text/plain")}
         },
         {
             kUTTypeRTF,
-            {CFSTR(".rtf")},
+            {CFSTR("rtf")},
             {CFSTR("text/rtf")}
         },
         {
             kUTTypeHTML,
-            {CFSTR(".html"), CFSTR(".htm")},
+            {CFSTR("html"), CFSTR("htm")},
             {CFSTR("text/html")}
         },
         {
             kUTTypeXML,
-            {CFSTR(".xml")},
+            {CFSTR("xml")},
             {CFSTR("text/xml")}
         },
         {
             CFSTR("com.netscape.javascript-​source"),
-            {CFSTR(".js"), CFSTR(".jscript"), CFSTR(".javascript")},
+            {CFSTR("js"), CFSTR("jscript"), CFSTR("javascript")},
             {CFSTR("text/javascript")}
         },
         {
             CFSTR("public.perl-script"),
-            {CFSTR(".pl"), CFSTR(".pm")},
+            {CFSTR("pl"), CFSTR("pm")},
             {CFSTR("text/x-perl-script")}
         },
         {
             CFSTR("public.python-script"),
-            {CFSTR(".py")},
+            {CFSTR("py")},
             {CFSTR("text/x-python-script")}
         },
         {
             CFSTR("public.ruby-script"),
-            {CFSTR(".rb"), CFSTR(".rbw")},
+            {CFSTR("rb"), CFSTR("rbw")},
             {CFSTR("text/ruby-script")}
         },
         {
             CFSTR("public.php-script"),
-            {CFSTR(".php"), CFSTR(".php3"), CFSTR(".php4"), CFSTR(".ph3"), CFSTR(".ph4"), CFSTR(".phtml")},
+            {CFSTR("php"), CFSTR("php3"), CFSTR("php4"), CFSTR("ph3"), CFSTR("ph4"), CFSTR("phtml")},
             {CFSTR("text/x-php-script"), CFSTR("text/php"), CFSTR("application/php")}
         },
         {
             CFSTR("com.sun.java-web-start"),
-            {CFSTR(".jnlp")},
+            {CFSTR("jnlp")},
             {CFSTR("application/x-java"), CFSTR("application/jnlp")}
         },
         {
             CFSTR("com.microsoft.windows-​executable"),
-            {CFSTR(".exe")},
+            {CFSTR("exe")},
             {CFSTR("application/x-msdownload")}
         },
         {
             CFSTR("com.microsoft.windows-​dynamic-link-library"),
-            {CFSTR(".dll")},
+            {CFSTR("dll")},
             {CFSTR("application/x-msdownload")}
         },
         {
             CFSTR("com.sun.java-archive"),
-            {CFSTR(".jar")},
+            {CFSTR("jar")},
             {CFSTR("application/java-archive")}
         },
         {
             CFSTR("com.apple.quartz-​composer-composition"),
-            {CFSTR(".qtz")},
+            {CFSTR("qtz")},
             {CFSTR("application/x-quartzcomposer")}
         },
         {
             CFSTR("org.gnu.gnu-tar-archive"),
-            {CFSTR(".gtar")},
+            {CFSTR("gtar")},
             {CFSTR("application/x-gtar")}
         },
         {
             CFSTR("public.tar-archive"),
-            {CFSTR(".tar")},
+            {CFSTR("tar")},
             {CFSTR("application/x-tar"), CFSTR("application/tar")}
         },
         {
             CFSTR("org.gnu.gnu-zip-archive"),
-            {CFSTR(".gz"), CFSTR(".gzip")},
+            {CFSTR("gz"), CFSTR("gzip")},
             {CFSTR("application/x-gzip"), CFSTR("application/gzip")}
         },
         {
             CFSTR("com.apple.binhex-archive"),
-            {CFSTR(".hqx")},
+            {CFSTR("hqx")},
             {CFSTR("application/mac-binhex40"), CFSTR("application/mac-binhex"), CFSTR("application/binhex")}
         },
         {
             CFSTR("com.apple.macbinary-​archive"),
-            {CFSTR(".bin")},
+            {CFSTR("bin")},
             {CFSTR("application/x-macbinary"), CFSTR("application/macbinary")}
         },
         {
             kUTTypeVCard,
-            {CFSTR(".vcf"), CFSTR(".vcard")},
+            {CFSTR("vcf"), CFSTR("vcard")},
             {CFSTR("text/x-vcard"), CFSTR("text/directory"), CFSTR("text/vcard")}
         },
         {
             kUTTypeJPEG,
-            {CFSTR(".jpg"), CFSTR(".jpeg")},
+            {CFSTR("jpg"), CFSTR("jpeg")},
             {CFSTR("image/jpeg")}
         },
         {
             kUTTypeJPEG2000,
-            {CFSTR(".jp2")},
+            {CFSTR("jp2")},
             {CFSTR("image/jp2")}
         },
         {
             kUTTypeTIFF,
-            {CFSTR(".tif"), CFSTR(".tiff")},
+            {CFSTR("tif"), CFSTR("tiff")},
             {CFSTR("image/tiff")}
         },
         {
             kUTTypePICT,
-            {CFSTR(".pic"), CFSTR(".pct"), CFSTR(".pict")},
+            {CFSTR("pic"), CFSTR("pct"), CFSTR("pict")},
             {CFSTR("image/pict"), CFSTR("image/x-pict"), CFSTR("image/x-macpict")}
         },
         {
             kUTTypePNG,
-            {CFSTR(".png")},
+            {CFSTR("png")},
             {CFSTR("image/png")}
         },
         {
             CFSTR("public.xbitmap-image"),
-            {CFSTR(".xbm")},
+            {CFSTR("xbm")},
             {CFSTR("image/x-quicktime")}
         },
         {
             kUTTypeQuickTimeImage,
-            {CFSTR(".qif"), CFSTR(".qtif")},
+            {CFSTR("qif"), CFSTR("qtif")},
             {CFSTR("image/x-quicktime")}
         },
         {
             kUTTypeQuickTimeMovie,
-            {CFSTR(".mov"), CFSTR(".qt")},
+            {CFSTR("mov"), CFSTR("qt")},
             {CFSTR("video/quicktime")}
         },
         {
             CFSTR("public.avi"),
-            {CFSTR(".avi"), CFSTR(".vfw")},
+            {CFSTR("avi"), CFSTR("vfw")},
             {CFSTR("video/avi"), CFSTR("video/msvideo"), CFSTR("video/x-msvideo")}
         },
         {
             kUTTypeMPEG,
-            {CFSTR(".mpg"), CFSTR(".mpeg"), CFSTR(".m75"), CFSTR(".m15")},
+            {CFSTR("mpg"), CFSTR("mpeg"), CFSTR("m75"), CFSTR("m15")},
             {CFSTR("video/mpg"), CFSTR("video/mpeg"), CFSTR("video/x-mpg"), CFSTR("video/x-mpeg")}
         },
         {
             kUTTypeMPEG4,
-            {CFSTR(".mp4")},
+            {CFSTR("mp4")},
             {CFSTR("video/mp4"), CFSTR("video/mp4v")}
         },
         {
             CFSTR("public.3gpp"),
-            {CFSTR(".3gp"), CFSTR(".3gpp")},
+            {CFSTR("3gp"), CFSTR("3gpp")},
             {CFSTR("video/3gpp"), CFSTR("audio/3gpp")}
         },
         {
             CFSTR("public.3gpp2"),
-            {CFSTR(".3g2"), CFSTR(".3gp2")},
+            {CFSTR("3g2"), CFSTR("3gp2")},
             {CFSTR("video/3gpp2"), CFSTR("audio/3gpp2")}
         },
         {
             kUTTypeMP3,
-            {CFSTR(".mp3")},
+            {CFSTR("mp3")},
             {CFSTR("audio/x-mpeg"), CFSTR("audio/mpeg"), CFSTR("audio/mpeg3"), CFSTR("audio/mpg"), CFSTR("audio/x-mpeg3"), CFSTR("audio/x-mpg"), CFSTR("audio/x-mp3")}
         },
         {
             CFSTR("public.ulaw-audio"),
-            {CFSTR(".au"), CFSTR(".ulw"), CFSTR(".snd")},
+            {CFSTR("au"), CFSTR("ulw"), CFSTR("snd")},
             {CFSTR("audio/basic"), CFSTR("audio/au"), CFSTR("audio/snd")}
         },
         {
             CFSTR("public.aifc-audio"),
-            {CFSTR(".aifc"), CFSTR(".aiff"), CFSTR(".aif")},
+            {CFSTR("aifc"), CFSTR("aiff"), CFSTR("aif")},
             {CFSTR("audio/x-aiff"), CFSTR("audio/aiff")}
         },
         {
             CFSTR("public.aiff-audio"),
-            {CFSTR(".aiff"), CFSTR(".aif")},
+            {CFSTR("aiff"), CFSTR("aif")},
             {CFSTR("audio/x-aiff"), CFSTR("audio/aiff")}
         },
         {
             CFSTR("com.pkware.zip-archive"),
-            {CFSTR(".zip")},
+            {CFSTR("zip")},
             {CFSTR("application/zip")}
+        },
+        {
+            CFSTR("public.x509-certificate"),
+            {CFSTR("cer")},
+            {CFSTR("application/x-x509-ca-cert")}
         },
     } };
     // clang-format on
