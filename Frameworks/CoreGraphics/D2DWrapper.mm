b7980f529f96df70f309d0592906ba0cf9bd5667
Henry Fox <henryfox@microsoft.com>
2016-10-06 16:10:57 -0700

    D2DFactory Wrapper (#1085)
    * D2DFactory Wrapper
    
    * Fix path for wrapper includes in project file.
    
    * Moving starboard to mm file.
    
    * Address CR feedback, remove useless code, clean up D2D factories.
    
    * Moving header file to private location
    
    * Factory helper should only create single threaded D2D factories.
    
    * Actually fix the wrapper header location.

---
+++ Frameworks/CoreGraphics/D2DWrapper.mm
@@ -0,0 +1,27 @@
+//******************************************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
+
+#import "D2DWrapper.h"
+#import <Starboard.h>
+
+using namespace Microsoft::WRL;
+
+// Helper for creating a D2DFactory
+ComPtr<ID2D1Factory> _CreateD2DFactoryInstance() {
+    ComPtr<ID2D1Factory> d2dFactory;
+    THROW_IF_FAILED(D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, __uuidof(ID2D1Factory), &d2dFactory));
+    return d2dFactory;
+}
