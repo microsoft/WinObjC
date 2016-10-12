083c369e3be8cca5ea83fb2de586a1d014e700bc
Yi Yang <yiyang@microsoft.com>
2016-10-12 14:05:40 -0700

    Fix convertPoint issue in UIScrollview and ensure two delegates are fired properly when decelerating.

--- Frameworks/QuartzCore/CALayer.mm
+++ Frameworks/QuartzCore/CALayer.mm
@@ -2562,7 +2562,7 @@ inline id _createRtProxy(Class cls, IInspectable* iface) {
 }
 
 inline WXUIElement* _getBackingXamlElementForCALayer(CALayer* layer) {
-    Microsoft::WRL::ComPtr<IInspectable> fromNode(GetCACompositor()->GetXamlLayoutElement([layer _presentationNode]));
+    Microsoft::WRL::ComPtr<IInspectable> fromNode(GetCACompositor()->GetXamlContentElement([layer _presentationNode]));
     return _createRtProxy([WXUIElement class], fromNode.Get());
 }
 
@@ -2586,6 +2586,10 @@ inline WXUIElement* _getBackingXamlElementForCALayer(CALayer* layer) {
         WFPoint* pointInFromLayer = [WXPointHelper fromCoordinates:point.x y:point.y];
         WFPoint* pointInToLayer = [transform transformPoint:pointInFromLayer];
         ret = { pointInToLayer.x, pointInToLayer.y };
+    }
+
+    if (DEBUG_VERBOSE) {
+        TraceVerbose(TAG, L"convertPoint: {%f, %f} to {%f, %f}",  point.x, point.y, ret.x, ret.y);
     }
 
     return ret;
