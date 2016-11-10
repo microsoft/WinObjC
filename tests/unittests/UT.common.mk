6cbc950a4740900cc114c89770f085de5a823c02
Dustin L. Howett <duhowett@microsoft.com>
2016-11-10 13:21:23 -0800

    [ut] Pull common logic out into a common makefile.

---
+++ tests/unittests/UT.common.mk
@@ -0,0 +1,100 @@
+#
+# Copyright (c) Microsoft. All rights reserved.
+#
+
+_UT_PROJECT_DIR := $(shell pwd)
+_UT_BASE_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+_UT_OUT_DIR := $(_UT_BASE_DIR)/../../build/Tests/UnitTests/$(UT_NAME)/OSX
+_UT_OBJ_DIR := $(_UT_OUT_DIR)/Objects
+_UT_FILENAME := $(UT_NAME).UnitTests.exe
+
+all: $(_UT_OUT_DIR)/$(_UT_FILENAME)
+.PHONY: all
+
+CLANG := clang++
+OSX_VERSION_MIN := 10.10
+
+_UT_OBJECTS := $(addsuffix .o,$(addprefix $(_UT_OBJ_DIR)/,$(subst $(_UT_PROJECT_DIR)/,,$(UT_FILES))))
+
+# Common shims for all tests.
+_UT_OBJECTS += $(_UT_OBJ_DIR)/windows.o $(_UT_OBJ_DIR)/IwMalloc.o $(_UT_OBJ_DIR)/LoggingNative.o
+ifeq ($(strip $(filter EntryPoint.cpp,$(UT_FILES))),)
+_UT_OBJECTS += $(_UT_OBJ_DIR)/EntryPoint.o
+endif
+
+_INC := $(_UT_BASE_DIR)/../frameworks/include $(_UT_BASE_DIR)/../frameworks/gtest $(_UT_BASE_DIR)/../frameworks/gtest/include 
+_INC += $(_UT_BASE_DIR)/../frameworks/OSXShims/include $(_UT_BASE_DIR)/../../include/xplat
+
+CXXFLAGS := -std=c++14
+CFLAGS := $(foreach i,$(_INC),-I $(i))
+CFLAGS += -DTARGET_OS_MAC
+CFLAGS += -DWINOBJCRT_IMPEXP=
+CFLAGS += -DLOGGING_IMPEXP=
+CFLAGS += -mmacosx-version-min=$(OSX_VERSION_MIN)
+CFLAGS += $(UT_CFLAGS)
+
+_UT_FRAMEWORKS := CoreFoundation Foundation $(UT_FRAMEWORKS)
+LDFLAGS := -mmacosx-version-min=$(OSX_VERSION_MIN)
+LDFLAGS += $(foreach o,$(_UT_FRAMEWORKS),-framework $(o))
+LDFLAGS += $(UT_LDFLAGS)
+
+ECHO_CC = @(echo "[ CC ] $^"; 
+ECHO_LD = @(echo "[ LD ] $@";
+ECHO_RES = @(echo "[ RES ] $^";
+ECHO_RESDIR = @(echo "[ DIR ] $^";
+ECHO_END = )
+
+$(_UT_OBJ_DIR)/IwMalloc.o : $(_UT_BASE_DIR)/../../Frameworks/WinObjCRT/MemoryManagement.cpp
+	@mkdir -p $(dir $@)
+	$(ECHO_CC)$(CLANG) -c -o $@ $(CFLAGS) $(CXXFLAGS) $<$(ECHO_END)
+
+$(_UT_OBJ_DIR)/windows.o : $(_UT_BASE_DIR)/../frameworks/OSXShims/src/windows.cpp
+	@mkdir -p $(dir $@)
+	$(ECHO_CC)$(CLANG) -c -o $@ $(CFLAGS) $(CXXFLAGS) $<$(ECHO_END)
+
+$(_UT_OBJ_DIR)/LoggingNative.o : $(_UT_BASE_DIR)/../frameworks/OSXShims/src/LoggingNative.mm
+	@mkdir -p $(dir $@)
+	$(ECHO_CC)$(CLANG) -c -o $@ $(CFLAGS) $(CXXFLAGS) -x objective-c++ $<$(ECHO_END)
+
+$(_UT_OBJ_DIR)/EntryPoint.o : $(_UT_BASE_DIR)/EntryPoint.cpp
+	@mkdir -p $(dir $@)
+	$(ECHO_CC)$(CLANG) -c -o $@ $(CFLAGS) $(CXXFLAGS) -x c++ $<$(ECHO_END)
+
+$(_UT_OBJ_DIR)/%.m.o : %.m
+	@mkdir -p $(dir $@)
+	$(ECHO_CC)$(CLANG) -c -o $@ $(CFLAGS) $(CXXFLAGS) -x objective-c++ $^$(ECHO_END)
+
+$(_UT_OBJ_DIR)/%.mm.o : %.mm
+	@mkdir -p $(dir $@)
+	$(ECHO_CC)$(CLANG) -c -o $@ $(CFLAGS) $(CXXFLAGS) -x objective-c++ $^$(ECHO_END)
+
+$(_UT_OBJ_DIR)/%.cpp.o : %.cpp
+	@mkdir -p $(dir $@)
+	$(ECHO_CC)$(CLANG) -c -o $@ $(CFLAGS) $(CXXFLAGS) -x c++ $^$(ECHO_END)
+
+.PHONY: clean
+clean:
+	-$(RM) -r $(_UT_OUT_DIR)/*
+
+_UT_RESOURCES := $(strip $(subst $(_UT_PROJECT_DIR)/,,$(UT_RESOURCES)))
+_UT_RESOURCE_DIRS := $(strip $(subst $(_UT_PROJECT_DIR)/,,$(UT_RESOURCE_DIRS)))
+export _UT_RESOURCES _UT_RESOURCE_DIRS
+
+$(_UT_OUT_DIR)/.resfiles.stamp: $(_UT_RESOURCES)
+ifneq ($(_UT_RESOURCES),)
+	$(ECHO_RES)cp -r $$_UT_RESOURCES "$(_UT_OUT_DIR)"$(ECHO_END)
+endif
+	@touch "$@"
+
+$(_UT_OUT_DIR)/.resdirs.stamp: $(_UT_RESOURCE_DIRS)
+ifneq ($(_UT_RESOURCE_DIRS),)
+	$(ECHO_RESDIR)for d in $$_UT_RESOURCE_DIRS; do \
+		rsync -r "$$d/" "$(_UT_OUT_DIR)/"; \
+	done$(ECHO_END)
+endif
+	@touch "$@"
+
+$(_UT_OUT_DIR)/$(_UT_FILENAME) : $(SHIMS) $(_UT_OBJECTS) $(_UT_OUT_DIR)/.resfiles.stamp $(_UT_OUT_DIR)/.resdirs.stamp
+	$(ECHO_LD)$(CLANG) -o $@ $(CFLAGS) $(CXXFLAGS) $(_UT_BASE_DIR)/../frameworks/gtest/src/gtest-all.cc \
+	$(LDFLAGS) $(_UT_OBJECTS)$(ECHO_END)
+	@echo "\nDone, please run from $(shell CDPATH="" cd "$(dir $@)"; pwd)/$(notdir $@)"
