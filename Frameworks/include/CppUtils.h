6f7e249ec37804ac7fb30bf09998793780516b0c
AJ Ballway <aballway@users.noreply.github.com>
2016-12-01 08:19:36 -0800

    Fix edge condition in lineFragmentRectForProposedRect (#1398)
    Handle case where multiple exclusion zones are on the same line and overlap. Also removes ScanlineCalculator and _CGPathFitRect.
    
    Fixes #1123

--- Frameworks/include/CppUtils.h
+++ Frameworks/include/CppUtils.h
@@ -24,11 +24,23 @@ inline bool operator==(const CFRange& lhs, const CFRange& rhs) {
     return lhs.location == rhs.location && lhs.length == rhs.length;
 }
 
+template <typename T>
+std::basic_ostream<T>& operator<<(std::basic_ostream<T>& os, const CFRange& range) {
+    os << "{ location: " << range.location << ", length: " << range.length << " }";
+    return os;
+}
+
 #pragma endregion
 
 #pragma region CGPoint
 inline bool operator==(const CGPoint& lhs, const CGPoint& rhs) {
     return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+template <typename T>
+std::basic_ostream<T>& operator<<(std::basic_ostream<T>& os, const CGPoint& point) {
+    os << "{ x: " << point.x << ", y: " << point.y << " }";
+    return os;
 }
 
 #pragma endregion
@@ -38,11 +50,23 @@ inline bool operator==(const CGSize& lhs, const CGSize& rhs) {
     return lhs.width == rhs.width && lhs.height == rhs.height;
 }
 
+template <typename T>
+std::basic_ostream<T>& operator<<(std::basic_ostream<T>& os, const CGSize& size) {
+    os << "{ width: " << size.width << ", height: " << size.height << " }";
+    return os;
+}
+
 #pragma endregion
 
 #pragma region CGRect
 inline bool operator==(const CGRect& lhs, const CGRect& rhs) {
     return lhs.origin == rhs.origin && lhs.size == rhs.size;
+}
+
+template <typename T>
+std::basic_ostream<T>& operator<<(std::basic_ostream<T>& os, const CGRect& rect) {
+    os << "{ origin: " << rect.origin << ", size: " << rect.size << " }";
+    return os;
 }
 
 #pragma endregion
