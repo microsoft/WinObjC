7e0c6ba5ee239bbc25061eb85b091976e41d922c
Dave Lamb <davelamb@microsoft.com>
2016-12-04 22:59:50 -0800

    Add additional UIControl events:
    TouchUpOutside
    TouchDragInside
    TouchDragOutside
    TouchDragEnter
    TouchDragExit
    
    Test added in ButtonsViewController in WOCCatalog.

--- samples/WOCCatalog/WOCCatalog/ButtonsViewController.m
+++ samples/WOCCatalog/WOCCatalog/ButtonsViewController.m
@@ -26,6 +26,45 @@ static const int TAG_SUBVIEW_UIBUTTON = 1;
     return self;
 }
 
+- (void)touchDown:(id)sender {
+    UIButton* button = (UIButton*)sender;
+
+    [button setTitle:@"Touch Down" forState:UIControlStateNormal];
+}
+
+- (void)touchUpInside:(id)sender {
+    UIButton* button = (UIButton*)sender;
+
+    [button setTitle:@"Touch Up Inside" forState:UIControlStateNormal];
+}
+
+- (void)touchUpOutside:(id)sender {
+    UIButton* button = (UIButton*)sender;
+
+    [button setTitle:@"Touch Up Outside" forState:UIControlStateNormal];
+}
+
+- (void)touchDragInside:(id)sender {
+    UIButton* button = (UIButton*)sender;
+
+    [button setTitle:@"Drag Inside" forState:UIControlStateNormal];
+}
+
+- (void)touchDragOutside:(id)sender {
+    UIButton* button = (UIButton*)sender;
+
+    [button setTitle:@"Drag Outside" forState:UIControlStateNormal];
+}
+
+// Since the following will get clobbered by other drag events, just output to the debug window.
+- (void)touchDragEnter:(id)sender {
+    NSLog(@"Drag Enter");
+}
+
+- (void)touchDragExit:(id)sender {
+    NSLog(@"Drag Exit");
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 
@@ -38,7 +77,7 @@ static const int TAG_SUBVIEW_UIBUTTON = 1;
 }
 
 - (NSInteger)tableView:(UITableView*)tableView numberOfRowsInSection:(NSInteger)section {
-    return 2; //[self.menuItems count];
+    return 3; //[self.menuItems count];
 }
 
 - (CGFloat)tableView:(UITableView*)tableView heightForRowAtIndexPath:(NSIndexPath*)indexPath {
@@ -79,6 +118,22 @@ static const int TAG_SUBVIEW_UIBUTTON = 1;
         [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         [button setTintColor:[UIColor whiteColor]];
         button.tag = TAG_SUBVIEW_UIBUTTON;
+        [cell addSubview:button];
+    } else if (indexPath.row == 2) {
+        UIButton* button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        button.frame = CGRectMake(5.0f, 5.0f, 200.0f, cell.frame.size.height - 5.0f);
+        
+        [button setTitle:@"UIControl Events" forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+
+        [button addTarget:self action:@selector(touchDown:) forControlEvents:UIControlEventTouchDown];
+        [button addTarget:self action:@selector(touchUpInside:) forControlEvents:UIControlEventTouchUpInside];
+        [button addTarget:self action:@selector(touchUpOutside:) forControlEvents:UIControlEventTouchUpOutside];
+        [button addTarget:self action:@selector(touchDragInside:) forControlEvents:UIControlEventTouchDragInside];
+        [button addTarget:self action:@selector(touchDragOutside:) forControlEvents:UIControlEventTouchDragOutside];
+        [button addTarget:self action:@selector(touchDragEnter:) forControlEvents:UIControlEventTouchDragEnter];
+        [button addTarget:self action:@selector(touchDragExit:) forControlEvents:UIControlEventTouchDragExit];
+
         [cell addSubview:button];
     }
 
