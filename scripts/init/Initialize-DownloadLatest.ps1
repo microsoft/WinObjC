885eb9d057fd517d0027c4cce76909123fbe994d
Brian Bowman <bbowman@microsoft.com>
2017-01-18 17:41:42 -0800

    Move many prebuilts and tool like things over to the /tools/ directory. Additionally rework /tools/ to build packages

---
+++ scripts/init/Initialize-DownloadLatest.ps1
@@ -0,0 +1,86 @@
+﻿Param(
+    [Parameter(Mandatory=$true)] [string] $outDir,
+    [Parameter(Mandatory=$true)] [string] $downloadUrl,
+    [Parameter(Mandatory=$true)] [string] $downloadName,    
+    [Parameter(Mandatory=$false)] [boolean] $unzip = $false
+)
+
+$ErrorActionPreference = "Stop"
+
+# Grab the VSS.NuGet toolset from VSO
+Add-Type -AssemblyName System.IO
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+$etagFile = Join-Path $outDir "$downloadName.ETag"
+$downloadPath = Join-Path $outDir "$downloadName.download"
+$downloadDest = Join-Path $outDir $downloadName
+$downloadDestTemp = Join-Path $outDir "$downloadName.tmp"
+$headers = @{}
+
+Write-Host -NoNewLine "Ensuring that $downloadName is up to date..."
+
+# If the destination folder doesn't exist, delete the ETag file if it exists
+if (!(Test-Path -PathType Container $downloadDest) -and (Test-Path -PathType Container $etagFile)) {
+    Remove-Item -Force $etagFile
+}
+
+if (Test-Path $etagFile)
+{
+    $headers.Add("If-None-Match", [System.IO.File]::ReadAllText($etagFile))
+}
+
+try 
+{
+    $response = Invoke-WebRequest -Headers $headers -Uri $downloadUrl -PassThru -OutFile $downloadPath -UseBasicParsing
+}
+catch [System.Net.WebException]
+{
+    $response = $_.Exception.Response
+}
+
+if ($response.StatusCode -eq 200)
+{
+    Unblock-File $downloadPath
+    [System.IO.File]::WriteAllText($etagFile, $response.Headers["ETag"])
+
+    if($unzip)
+    {
+        # Extract to a temp folder
+        if (Test-Path -PathType Container $downloadDestTemp)
+        {
+            [System.IO.Directory]::Delete($downloadDestTemp, $true)
+        }
+
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($downloadPath, $downloadDestTemp)
+        Remove-Item $downloadPath
+    }
+    else
+    {
+        $downloadDestTemp = $downloadPath;
+    }
+
+    # Delete and rename to final dest
+    if (Test-Path -PathType Container $downloadDest)
+    {
+        [System.IO.Directory]::Delete($downloadDest, $true)
+    }
+
+    Move-Item -Force $downloadDestTemp $downloadDest
+    Write-Host "Updated $downloadName"
+}
+elseif ($response.StatusCode -eq 304)
+{
+    Write-Host "Done"
+}
+else
+{
+	Write-Host
+    Write-Warning "Failed to fetch updated NuGet tools from $downloadUrl"
+    if (!(Test-Path $downloadDest)) {
+        throw "$downloadName was not found at $downloadDest"
+    } else {
+        Write-Warning "$downloadName may be out of date"
+    }
+}
+
+return $downloadDest
---
+++ scripts/init/Initialize-InstallFromNuget.ps1
@@ -0,0 +1,33 @@
+﻿Param(
+    [Parameter(Mandatory=$true)] [string] $outDir,
+    [Parameter(Mandatory=$true)] [string] $downloadFeed,
+    [Parameter(Mandatory=$true)] [string] $packageName,
+    [Parameter(Mandatory=$true)] [string] $targetFileName
+)
+
+$ErrorActionPreference = "Stop"
+
+$downloadDest = Join-Path $outDir $targetFileName
+
+$tempFolder = "$env:temp\Package-$(Get-Date -format 'yyyy-MM-dd_hh-mm-ss')"
+New-Item $tempFolder -ItemType Directory
+
+& "$outDir\nuget.exe" install $packageName -source $downloadFeed -OutputDirectory `"$tempFolder`" -Prerelease -NonInteractive
+
+$authHelper = Get-ChildItem -Filter $targetFileName -Path $tempFolder -Recurse
+
+if(-not $authHelper)
+{
+    Write-Warning "Failed to fetch updated $targetFileName from $downloadFeed"
+    if (!(Test-Path $downloadDest)) {
+        throw "$packageName was not found at $downloadFeed"
+    } else {
+        Write-Warning "$targetFileName may be out of date"
+    }
+}
+else
+{
+   Copy-Item -Path $authHelper.FullName -Destination $outDir
+}
+
+Remove-Item $tempFolder -Force -Recurse
