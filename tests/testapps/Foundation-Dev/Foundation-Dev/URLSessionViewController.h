7b52eb647fae6447dd114289bed5abf0eb8c0326
AJ Ballway <aballway@users.noreply.github.com>
2017-01-26 11:54:48 -0800

    Migrate foundation tests out of woccatalog (#1826)
    Moves Foundation Tests, which includes task_info, URL Session, and UserDefaults, and moves them to a new test app Foundation-Dev in tests/testapps
    
    Fixes #1577
    
    * Migrate foundation tests out of woccatalog
    
    * Even more relative path

--- tests/testapps/Foundation-Dev/Foundation-Dev/URLSessionViewController.h
+++ tests/testapps/Foundation-Dev/Foundation-Dev/URLSessionViewController.h
@@ -1,6 +1,6 @@
 //******************************************************************************
 //
-// Copyright (c) 2015 Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 //
 // This code is licensed under the MIT License (MIT).
 //
@@ -14,8 +14,7 @@
 //
 //******************************************************************************
 
-#import "MenuTableViewController.h"
+#import <UIKit/UIKit.h>
 
-@interface FoundationsViewController : MenuTableViewController
-
+@interface URLSessionViewController : UIViewController
 @end
