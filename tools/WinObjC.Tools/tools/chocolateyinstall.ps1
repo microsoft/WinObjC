8248188fc373815b222ec7d1f16e7df109c8c06b
Brian Bowman <bbowman@microsoft.com>
2017-02-27 15:28:18 -0800

    s

--- tools/WinObjC.Tools/tools/chocolateyinstall.ps1
+++ tools/WinObjC.Tools/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
 
-$ErrorActionPreference = 'SilentlyContinue';
+$ErrorActionPreference = 'Stop';
 
 $packageName = 'WinObjC.tools' # arbitrary name for the package, used in messages
 $registryUninstallerKeyName = 'WinObjC.tools' #ensure this is the value in the registry
@@ -15,49 +15,11 @@ Write-Host "Adding `'$toolsDir`' to the path and the current shell path"
 Install-ChocolateyPath "$toolsDir"
 $env:Path = "$($env:Path);$toolsDir"
 
-#
-# TODO: Remove 2015 support when VS2017 ships. At that point, treat errors as failures since only one version will be supported.
-#
+Write-Host "Installing Objective-C Visual Studio 2017 Extension ... "
+Install-ChocolateyVsixPackage -PackageName "objc-syntax-highlighting" -VsixUrl "file:///$toolsDir/objc-language-services-VS2017.vsix"
 
-Try
-{
-    Write-Host "Installing Objective-C Visual Studio 2015 Extension ... "
-    Install-ChocolateyVsixPackage -PackageName "objc-syntax-highlighting" -VsixUrl "file:///$toolsDir/objc-syntax-highlighting.vsix" -VsVersion 14
-}
-Catch [Exception]
-{
-    Write-Warning "$_"
-}
-
-Try
-{
-    Write-Host "Installing Nugetizer Visual Studio 2015 Extension ... "
-    Install-ChocolateyVsixPackage -PackageName "Nugetizer3000" -VsixUrl "file:///$toolsDir/NuGetizer3000-VS2015.vsix" -VsVersion 14
-}
-Catch [Exception]
-{
-    Write-Warning "$_"
-}
-
-Try
-{
-    Write-Host "Installing Objective-C Visual Studio 2017 Extension ... "
-    Install-ChocolateyVsixPackage -PackageName "objc-syntax-highlighting" -VsixUrl "file:///$toolsDir/objc-language-services-VS2017.vsix" -VsVersion 15
-}
-Catch [Exception]
-{
-    Write-Warning "$_"
-}
-
-Try
-{
-    Write-Host "Installing Nugetizer Visual Studio 2017 Extension ... "
-    Install-ChocolateyVsixPackage -PackageName "Nugetizer3000" -VsixUrl "http://bit.ly/nugetizer-2017" -VsVersion 15
-}
-Catch [Exception]
-{
-    Write-Warning "$_"
-}
+Write-Host "Installing Nugetizer Visual Studio 2017 Extension ... "
+Install-ChocolateyVsixPackage -PackageName "Nugetizer3000" -VsixUrl "http://bit.ly/nugetizer-2017"
 
 # if removing $url64, please remove from here
 # despite the name "Install-ChocolateyZipPackage" this also works with 7z archives
