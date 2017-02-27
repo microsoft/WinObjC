a6108b1ab63fe6677fc3de2e465a11edfe94bbed
Brian Bowman <bbowman@microsoft.com>
2017-02-27 11:51:51 -0800

    Redistribute nugetizer vsix and rework chocolatey install script to be less failure prone for systems that don't have latest visual studios

--- tools/WinObjC.Tools/tools/chocolateyinstall.ps1
+++ tools/WinObjC.Tools/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
 
 # stop on all errors
-$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'SilentlyContinue';
 
 
 $packageName = 'WinObjC.tools' # arbitrary name for the package, used in messages
@@ -17,17 +17,45 @@ Write-Host "Adding `'$toolsDir`' to the path and the current shell path"
 Install-ChocolateyPath "$toolsDir"
 $env:Path = "$($env:Path);$toolsDir"
 
-Write-Host "Installing Objective-C Visual Studio 2015 Extension ... "
-Install-ChocolateyVsixPackage -PackageName "objc-syntax-highlighting" -VsixUrl "file:///$toolsDir/objc-syntax-highlighting.vsix"
+Try
+{
+    Write-Host "Installing Objective-C Visual Studio 2015 Extension ... "
+    Install-ChocolateyVsixPackage -PackageName "objc-syntax-highlighting" -VsixUrl "file:///$toolsDir/objc-syntax-highlighting.vsix" -VsVersion 14
+}
+Catch [Exception]
+{
+    Write-Warning "$_"
+}
 
-Write-Host "Installing Objective-C Visual Studio 2017 Extension ... "
-Install-ChocolateyVsixPackage -PackageName "objc-syntax-highlighting" -VsixUrl "file:///$toolsDir/objc-language-services-VS2017.vsix"
+Try
+{
+    Write-Host "Installing Nugetizer Visual Studio 2015 Extension ... "
+    Install-ChocolateyVsixPackage -PackageName "Nugetizer3000" -VsixUrl "file:///$toolsDir/NuGetizer3000-VS2015.vsix" -VsVersion 14
+}
+Catch [Exception]
+{
+    Write-Warning "$_"
+}
 
-Write-Host "Installing Nugetizer Visual Studio 2015 Extension ... "
-Install-ChocolateyVsixPackage -PackageName "Nugetizer3000" -VsixUrl "http://bit.ly/nugetizer2015"
+Try
+{
+    Write-Host "Installing Objective-C Visual Studio 2017 Extension ... "
+    Install-ChocolateyVsixPackage -PackageName "objc-syntax-highlighting" -VsixUrl "file:///$toolsDir/objc-language-services-VS2017.vsix" -VsVersion 14
+}
+Catch [Exception]
+{
+    Write-Warning "$_"
+}
 
-Write-Host "Installing Nugetizer Visual Studio 2017 Extension ... "
-Install-ChocolateyVsixPackage -PackageName "Nugetizer3000" -VsixUrl "http://bit.ly/nugetizer2017"
+Try
+{
+    Write-Host "Installing Nugetizer Visual Studio 2017 Extension ... "
+    Install-ChocolateyVsixPackage -PackageName "Nugetizer3000" -VsixUrl "file:///$toolsDir/NuGetizer3000-VS2017.vsix" -VsVersion 15
+}
+Catch [Exception]
+{
+    Write-Warning "$_"
+}
 
 # if removing $url64, please remove from here
 # despite the name "Install-ChocolateyZipPackage" this also works with 7z archives
