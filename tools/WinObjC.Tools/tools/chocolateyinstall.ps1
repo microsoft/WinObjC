c08d59e65ce3022562abc962d464e83c9ac3fe56
Brian Bowman <bbowman@microsoft.com>
2017-03-02 14:29:33 -0800

    Redistribute nugetizer vsix and rework chocolatey install script to b… (#2105)
    * Redistribute nugetizer vsix and rework chocolatey install script to be less failure prone for systems that don't have latest visual studios

--- tools/WinObjC.Tools/tools/chocolateyinstall.ps1
+++ tools/WinObjC.Tools/tools/chocolateyinstall.ps1
@@ -1,8 +1,6 @@
 ﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
 
-# stop on all errors
 $ErrorActionPreference = 'Stop';
-
 
 $packageName = 'WinObjC.tools' # arbitrary name for the package, used in messages
 $registryUninstallerKeyName = 'WinObjC.tools' #ensure this is the value in the registry
@@ -17,17 +15,11 @@ Write-Host "Adding `'$toolsDir`' to the path and the current shell path"
 Install-ChocolateyPath "$toolsDir"
 $env:Path = "$($env:Path);$toolsDir"
 
-Write-Host "Installing Objective-C Visual Studio 2015 Extension ... "
-Install-ChocolateyVsixPackage -PackageName "objc-syntax-highlighting" -VsixUrl "file:///$toolsDir/objc-syntax-highlighting.vsix"
-
 Write-Host "Installing Objective-C Visual Studio 2017 Extension ... "
 Install-ChocolateyVsixPackage -PackageName "objc-syntax-highlighting" -VsixUrl "file:///$toolsDir/objc-language-services-VS2017.vsix"
 
-Write-Host "Installing Nugetizer Visual Studio 2015 Extension ... "
-Install-ChocolateyVsixPackage -PackageName "Nugetizer3000" -VsixUrl "http://bit.ly/nugetizer2015"
-
 Write-Host "Installing Nugetizer Visual Studio 2017 Extension ... "
-Install-ChocolateyVsixPackage -PackageName "Nugetizer3000" -VsixUrl "http://bit.ly/nugetizer2017"
+Install-ChocolateyVsixPackage -PackageName "Nugetizer3000" -VsixUrl "http://bit.ly/nugetizer-2017"
 
 # if removing $url64, please remove from here
 # despite the name "Install-ChocolateyZipPackage" this also works with 7z archives
