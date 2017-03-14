5eecfcf445f5a2f992707cf10ebe9a36da6a4d3e
Dustin L. Howett <duhowett@microsoft.com>
2017-03-14 15:54:45 -0700

    Add tests for an image as src/dest.

--- tests/unittests/CoreGraphics.drawing/CGContextBlendModeTests.cpp
+++ tests/unittests/CoreGraphics.drawing/CGContextBlendModeTests.cpp
@@ -15,6 +15,8 @@
 //******************************************************************************
 
 #include "DrawingTest.h"
+#include "DrawingTestConfig.h"
+#include "ImageHelpers.h"
 
 struct CGNamedBlendMode {
     const char* name;
@@ -62,31 +64,21 @@ CGNamedBlendMode compositionModes[] = {
     { "kCGBlendModePlusDarker", kCGBlendModePlusDarker },
 };
 
-class CGContextBlendMode : public WhiteBackgroundTest<>, public ::testing::WithParamInterface<::testing::tuple<bool, CGNamedBlendMode>> {
-    CFStringRef CreateOutputFilename() {
-        const char* blendModeName = ::testing::get<1>(GetParam()).name;
-        bool useTransparencyLayer = ::testing::get<0>(GetParam());
-        return CFStringCreateWithFormat(nullptr, nullptr, CFSTR("TestImage.CGContext.Blending_%s%s.png"), useTransparencyLayer ? "Layered_" : "", blendModeName);
-    }
-};
-
-DRAW_TEST_P(CGContextBlendMode, OverlappedRects) {
-    CGContextRef context = GetDrawingContext();
-    CGRect bounds = GetDrawingBounds();
-    CGBlendMode blendMode = ::testing::get<1>(GetParam()).blendMode;
-    bool useTransparencyLayer = ::testing::get<0>(GetParam());
-
-    bounds = CGRectInset(bounds, 8.f, 8.f);
-
+static void __drawLeftStack(CGContextRef context, CGRect bounds, int variant = 0) {
     CGFloat unitHeight = bounds.size.height / 4.f;
 
-    CGContextSetLineWidth(context, 6.0f);
+    CGRect cursor = _CGRectCenteredOnPoint({ 2.f * bounds.size.width / 3.f, unitHeight },
+                                           { bounds.origin.x + (bounds.size.width / 3.f), bounds.origin.y + (unitHeight / 2.f) });
 
-    // Draw two rects and two lines, at both alphas.
-    CGRect cursor = _CGRectCenteredOnPoint({2.f * bounds.size.width / 3.f, unitHeight}, {bounds.origin.x +(bounds.size.width / 3.f), bounds.origin.y + (unitHeight / 2.f)});
-
+    CGContextSaveGState(context);
     CGContextSetRGBFillColor(context, .25, .71, .95, 1.0);
-    CGContextSetRGBStrokeColor(context, .25, .71, .95, 1.0);
+    if (variant == 0) {
+        // variant 0 = blue lines, blue boxes; variant 1 = dark red lines, blue boxes.
+        // This lets us test "lighten" more effectively.
+        CGContextSetRGBStrokeColor(context, .25, .71, .95, 1.0);
+    } else {
+        CGContextSetRGBStrokeColor(context, .45, 0, 0, 1.0);
+    }
 
     CGContextFillRect(context, cursor);
     cursor.origin.y += unitHeight;
@@ -96,22 +88,27 @@ DRAW_TEST_P(CGContextBlendMode, OverlappedRects) {
     cursor.origin.y += unitHeight;
 
     CGContextSetRGBFillColor(context, .25, .71, .95, 0.5);
-    CGContextSetRGBStrokeColor(context, .25, .71, .95, 0.5);
+    if (variant == 0) {
+        CGContextSetRGBStrokeColor(context, .25, .71, .95, 0.5);
+    } else {
+        CGContextSetRGBStrokeColor(context, .45, 0, 0, 0.5);
+    }
 
     CGContextFillRect(context, cursor);
     cursor.origin.y += unitHeight;
     CGContextMoveToPoint(context, CGRectGetMinX(cursor), CGRectGetMinY(cursor));
     CGContextAddLineToPoint(context, CGRectGetMaxX(cursor), CGRectGetMaxY(cursor));
     CGContextStrokePath(context);
-    cursor.origin.y += unitHeight;
+    CGContextRestoreGState(context);
+}
 
-    CGContextSetBlendMode(context, blendMode);
+static void __drawRightStackPink(CGContextRef context, CGRect bounds) {
+    CGFloat unitHeight = bounds.size.height / 4.f;
 
-    if (useTransparencyLayer) {
-        CGContextBeginTransparencyLayer(context, nullptr);
-    }
+    CGRect cursor = _CGRectCenteredOnPoint({ 2.f * bounds.size.width / 3.f, unitHeight },
+                                    { bounds.origin.x + (2.f * bounds.size.width / 3.f), bounds.origin.y + (unitHeight / 2.f) });
 
-    cursor = _CGRectCenteredOnPoint({2.f * bounds.size.width / 3.f, unitHeight}, {bounds.origin.x +(2.f * bounds.size.width / 3.f), bounds.origin.y + (unitHeight / 2.f)});
+    CGContextSaveGState(context);
     CGContextSetRGBFillColor(context, .95, .25, .66, 1.0);
     CGContextSetRGBStrokeColor(context, .95, .25, .66, 1.0);
 
@@ -130,7 +127,41 @@ DRAW_TEST_P(CGContextBlendMode, OverlappedRects) {
     CGContextMoveToPoint(context, CGRectGetMinX(cursor), CGRectGetMaxY(cursor));
     CGContextAddLineToPoint(context, CGRectGetMaxX(cursor), CGRectGetMinY(cursor));
     CGContextStrokePath(context);
-    cursor.origin.y += unitHeight;
+
+    CGContextRestoreGState(context);
+}
+
+class CGContextBlendMode : public WhiteBackgroundTest<>, public ::testing::WithParamInterface<::testing::tuple<bool, CGNamedBlendMode>> {
+    CFStringRef CreateOutputFilename() {
+        const char* blendModeName = ::testing::get<1>(GetParam()).name;
+        bool useTransparencyLayer = ::testing::get<0>(GetParam());
+        return CFStringCreateWithFormat(nullptr,
+                                        nullptr,
+                                        CFSTR("TestImage.CGContext.Blending_%s%s.png"),
+                                        useTransparencyLayer ? "Layered_" : "",
+                                        blendModeName);
+    }
+};
+
+DRAW_TEST_P(CGContextBlendMode, OverlappedRects) {
+    CGContextRef context = GetDrawingContext();
+    CGRect bounds = GetDrawingBounds();
+    CGBlendMode blendMode = ::testing::get<1>(GetParam()).blendMode;
+    bool useTransparencyLayer = ::testing::get<0>(GetParam());
+
+    bounds = CGRectInset(bounds, 8.f, 8.f);
+
+    CGContextSetLineWidth(context, 6.0f);
+
+    __drawLeftStack(context, bounds);
+
+    CGContextSetBlendMode(context, blendMode);
+
+    if (useTransparencyLayer) {
+        CGContextBeginTransparencyLayer(context, nullptr);
+    }
+
+    __drawRightStackPink(context, bounds);
 
     if (useTransparencyLayer) {
         CGContextEndTransparencyLayer(context);
@@ -154,3 +185,65 @@ INSTANTIATE_TEST_CASE_P(OperatorBlendModes,
                         CGContextBlendMode,
                         ::testing::Combine(::testing::Values(false, true), ::testing::ValuesIn(blendOperators)));
 #endif
+
+class CGContextBlendModeImage : public WhiteBackgroundTest<>, public ::testing::WithParamInterface<CGNamedBlendMode> {
+    CFStringRef CreateOutputFilename() {
+        const char* blendModeName = GetParam().name;
+        // This works around the fact that TAEF cannot give us test case names.
+        // We have to divine them from the test full name. :(
+        auto testFullName = GetTestFullName();
+        bool source = (testFullName.find("ImageAsSource", 0) != std::string::npos);
+        return CFStringCreateWithFormat(nullptr, nullptr, CFSTR("TestImage.CGContext.Blending_Image%s_%s.png"), source ? "Source" : "Destination", blendModeName);
+    }
+
+public:
+    static woc::StrongCF<CGImageRef> s_testImage;
+    static void SetUpTestCase() {
+        auto drawingConfig = DrawingTestConfig::Get();
+        auto testFilename = woc::MakeStrongCF<CFStringRef>(_CFStringCreateWithStdString(drawingConfig->GetResourcePath("winobjc.png")));
+        s_testImage.attach(_CGImageCreateFromPNGFile(testFilename));
+    }
+
+    static void TearDownTestCase() {
+        s_testImage.attach(nullptr);
+    }
+};
+woc::StrongCF<CGImageRef> CGContextBlendModeImage::s_testImage;
+
+DRAW_TEST_P(CGContextBlendModeImage, ImageAsSource) {
+    CGContextRef context = GetDrawingContext();
+    CGRect bounds = GetDrawingBounds();
+    CGBlendMode blendMode = GetParam().blendMode;
+
+    bounds = CGRectInset(bounds, 8.f, 8.f);
+
+    CGContextSetLineWidth(context, 6.0f);
+
+    __drawLeftStack(context, bounds, 1);
+
+    CGContextSetBlendMode(context, blendMode);
+
+    CGContextDrawImage(context, GetDrawingBounds(), s_testImage);
+}
+
+DRAW_TEST_P(CGContextBlendModeImage, ImageAsDestination) {
+    CGContextRef context = GetDrawingContext();
+    CGRect bounds = GetDrawingBounds();
+    CGBlendMode blendMode = GetParam().blendMode;
+
+    bounds = CGRectInset(bounds, 8.f, 8.f);
+
+    CGContextSetLineWidth(context, 6.0f);
+
+    CGContextDrawImage(context, GetDrawingBounds(), s_testImage);
+
+    CGContextSetBlendMode(context, blendMode);
+
+    CGContextBeginTransparencyLayer(context, nullptr);
+    __drawRightStackPink(context, bounds);
+    CGContextEndTransparencyLayer(context);
+}
+
+INSTANTIATE_TEST_CASE_P(CompositionModes, CGContextBlendModeImage, ::testing::ValuesIn(compositionModesWithoutPlusDarker));
+
+INSTANTIATE_TEST_CASE_P(AdobePDFBlendModes, CGContextBlendModeImage, ::testing::ValuesIn(pdfBlendModes));
