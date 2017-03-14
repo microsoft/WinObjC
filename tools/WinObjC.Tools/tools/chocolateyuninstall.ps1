62fbd0f89a03befb942c35b5c273d28c683db472
Brian Bowman <bbowman@microsoft.com>
2017-03-14 11:02:16 -0700

    Uninstall and handle process blocking message

--- tools/WinObjC.Tools/tools/chocolateyuninstall.ps1
+++ tools/WinObjC.Tools/tools/chocolateyuninstall.ps1
@@ -1,22 +1,35 @@
-﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
-
-# stop on all errors
-$ErrorActionPreference = 'Stop';
-
-# Auto Uninstaller should be able to detect and handle registry uninstalls (if it is turned on, it is in preview for 0.9.9).
+﻿#NOTE: Ple$ErrorActionPreference = 'Stop';
 
 $packageName = 'WinObjC.tools'
-$registryUninstallerKeyName = 'WinObjC.tools' #ensure this is the value in the registry
-$installerType = 'EXE'
-$silentArgs = '/S'
-$validExitCodes = @(0)
+$silentArgs = '' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-#$osBitness = Get-ProcessorBits
- 
-#if ($osBitness -eq 64) {
-#  $file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
-#} else {
-#  $file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
-#} 
-#	
-#Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+$installPath = & $toolsDir\vswhere.exe -latest -property installationPath
+
+if(!$installPath) {
+    throw "There was an error finding latest Visual Studio Install location. Please make sure Visual Studio (2017 or later) is installed correctly."
+}
+
+$vsixInstaller = gci -File -Recurse -Filter vsixinstaller.exe -Path $installPath
+
+Write-Host "Uninstalling Objective-C Visual Studio 2017 Extension ... " -ForegroundColor Blue
+
+$result = & $vsixInstaller.FullName /q /uninstall:VSIX..9c35fff1-f084-44c1-a38e-68c707163aa2
+if($result -eq 2004) { #2004: Blocking Process (need to close VS)
+    throw "A process is blocking the uninstallation of the Visual Studio Extension. Please close all Visual Studio instances and try again."
+}
+
+if($result -gt 0 -and $result -ne 1002) { #1002: Not installed
+    throw "There was an error uninstalling the Objective-C Visual Studio 2017 Extension. The exit code returned was $result."
+}
+
+Write-Host "Uninstalling Nugetizer Visual Studio 2017 Extension ... " -ForegroundColor Blue
+$result =  & $vsixInstaller.FullName /q /uninstall:NuGet.Packaging
+if($result -eq 2004) { #2004: Blocking Process (need to close VS)
+    throw "A process is blocking the uninstallation of the Visual Studio Extension. Please close all Visual Studio instances and try again."
+}
+
+if($result -gt 0 -and $result -ne 1002) { #1002: Not installed
+    throw "There was an error uninstalling the Objective-C Visual Studio 2017 Extension. The exit code returned was $result."
+}
