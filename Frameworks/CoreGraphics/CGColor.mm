ef9b2cd6c25d8f08186b4b1b2fd8d4045f48dce0
Jeyaram Jeyaraj <jejeyara@microsoft.com>
2017-04-03 14:36:39 -0700

    Update API status for CGColorGetConstantColor

--- Frameworks/CoreGraphics/CGColor.mm
+++ Frameworks/CoreGraphics/CGColor.mm
@@ -27,21 +27,17 @@ const CFStringRef kCGColorClear = CFSTR("CLEAR");
 static IWLazyClassLookup __LazyUIColor("UIColor");
 
 /**
- @Status Caveat
- @Notes Limited constants supported
+ @Status Interoperable
 */
 CGColorRef CGColorGetConstantColor(CFStringRef name) {
     CGColorRef ret = nullptr;
-    woc::StrongCF<CGColorSpaceRef> colorspace;
+    woc::StrongCF<CGColorSpaceRef> colorspace{ woc::MakeStrongCF<CGColorSpaceRef>(CGColorSpaceCreateDeviceGray()) };
     if (CFEqual(kCGColorBlack, name)) {
         ret = static_cast<CGColorRef>([[__LazyUIColor blackColor] CGColor]);
-        colorspace = woc::MakeStrongCF<CGColorSpaceRef>(CGColorSpaceCreateDeviceGray());
     } else if (CFEqual(kCGColorWhite, name)) {
         ret = static_cast<CGColorRef>([[__LazyUIColor whiteColor] CGColor]);
-        colorspace = woc::MakeStrongCF<CGColorSpaceRef>(CGColorSpaceCreateDeviceGray());
     } else if (CFEqual(kCGColorClear, name)) {
         ret = static_cast<CGColorRef>([[__LazyUIColor clearColor] CGColor]);
-        colorspace = woc::MakeStrongCF<CGColorSpaceRef>(CGColorSpaceCreateDeviceGray());
     } else {
         UNIMPLEMENTED_WITH_MSG("CGColorGetConstantColor does not support color %s", CFStringGetCStringPtr(name, kCFStringEncodingUTF8));
     }
