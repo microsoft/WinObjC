7c06040c0b21785faadaa2dbbedb33b51bad2401
Brian Bowman <bbowman@microsoft.com>
2017-04-04 13:52:41 -0700

    Unify all WinObjC Versioning
    This changes simplifies and unifies all versioning to be based on Git Tags.
    This removes the need for the version file and no longer requires master packages
    to be built with a version override (though they still can).
    
    vcxproj, csproj, and VSIX projects all get automatically versioned in a concsistent
    way and have more metadata like the Copyright and description correctly filled out.

--- tools/PropSchemaGen/Properties/AssemblyInfo.cs
+++ tools/PropSchemaGen/Properties/AssemblyInfo.cs
@@ -5,12 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PropSchemaGen")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PropSchemaGen")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("94933be4-0c74-4bff-9bfb-4fb0656f3095")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- tools/xib2xaml/xib2xaml/Properties/AssemblyInfo.cs
+++ tools/xib2xaml/xib2xaml/Properties/AssemblyInfo.cs
@@ -21,12 +21,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Xib2Xaml")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Xib2Xaml")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -39,17 +34,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("3fb02757-dec0-40a5-a90d-4b5b59718397")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
