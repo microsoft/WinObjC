9cfd34731726e1d957943ff393da6c3f985a6f38
Brian Bowman <bbowman@microsoft.com>
2017-04-06 18:37:34 -0700

    Unify all WinObjC Versioning (#2430)
    * Unify all WinObjC Versioning
    
    This changes simplifies and unifies all versioning to be based on Git Tags.
    This removes the need for the version file and no longer requires master packages
    to be built with a version override (though they still can).
    
    vcxproj, csproj, and VSIX projects all get automatically versioned in a consistent
    way and have more metadata like the Copyright and description correctly filled out.

--- tools/AssetCatalogBuilder/Properties/AssemblyInfo.cs
+++ tools/AssetCatalogBuilder/Properties/AssemblyInfo.cs
@@ -5,12 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AssetCatalogBuilder")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AssetCatalogBuilder")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- tools/ClangCompileTask/Properties/AssemblyInfo.cs
+++ tools/ClangCompileTask/Properties/AssemblyInfo.cs
@@ -5,12 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClassLibrary1")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClassLibrary1")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bd58c132-64ce-40cc-b7c6-a8a83b7d6a5e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
