39350a547a15de44b0c9d233b7b04afbff17350a
Ben Viglietta <benvi@microsoft.com>
2017-04-07 14:57:20 -0700

    Eliminate unnecessary warning when building on ARM

--- Frameworks/include/ShareSheetControl.h
+++ Frameworks/include/ShareSheetControl.h
@@ -24,10 +24,20 @@ class __declspec(novtable) __declspec(uuid("5CF22688-7892-4CE0-ACA4-3EA01A0EDDDB
 public:
     virtual ~ShareSheetControl() = default;
 
+// __stdcall ignored on ARM -- fine, no need for a warning
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wignored-attributes"
+#endif
+
     STDMETHOD(addLink)(const wchar_t* url) = 0;
     STDMETHOD(addImage)(IInspectable* image) = 0;
     STDMETHOD(setInitialText)(const wchar_t* text) = 0;
     STDMETHOD(show)() = 0;
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 };
 
 SOCIAL_XAML_EXPORT HRESULT XamlCreateShareSheet(const wchar_t* token, ShareSheetControl** sheet);
