ba2e9f409b3e2e4643ae1edaebc0e5e6f48cba19
Jingtao Huang <jihua@microsoft.com>
2017-04-18 16:26:06 -0700

    pragma once

--- Frameworks/Foundation/NSObjectInternal.h
+++ Frameworks/Foundation/NSObjectInternal.h
@@ -14,6 +14,8 @@
 //
 //******************************************************************************
 
+#pragma once
+
 #import <Foundation/NSObject.h>
 
 long _throwUnrecognizedSelectorException(id self, Class isa, SEL sel);
