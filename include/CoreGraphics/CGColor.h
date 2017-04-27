6a7b805c1dfdfede9f70e304118fceb99f107927
Jeyaram Jeyaraj <jejeyara@microsoft.com>
2017-04-26 19:24:37 -0700

    Implement CGColor generic RGBA and Grayscale creation APIs.
    - Implemented CGColorSpaceCreateDeviceRGB and CGColorSpaceCreateDeviceGray
    - Added Unittests for the functions
    Fixes #2614

--- include/CoreGraphics/CGColor.h
+++ include/CoreGraphics/CGColor.h
@@ -1,6 +1,6 @@
 //******************************************************************************
 //
-// Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 //
 // This code is licensed under the MIT License (MIT).
 //
@@ -40,3 +40,5 @@ COREGRAPHICS_EXPORT size_t CGColorGetNumberOfComponents(CGColorRef color);
 COREGRAPHICS_EXPORT const CGFloat* CGColorGetComponents(CGColorRef color);
 COREGRAPHICS_EXPORT CGPatternRef CGColorGetPattern(CGColorRef color);
 COREGRAPHICS_EXPORT CFTypeID CGColorGetTypeID();
+COREGRAPHICS_EXPORT CGColorRef CGColorCreateGenericRGB(CGFloat red, CGFloat green, CGFloat blue, CGFloat alpha);
+COREGRAPHICS_EXPORT CGColorRef CGColorCreateGenericGray(CGFloat gray, CGFloat alpha);
