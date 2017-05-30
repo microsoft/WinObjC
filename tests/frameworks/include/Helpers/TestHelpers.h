1ef766a890e3e88acf7cc8d4c4cd7571c20da107
Dustin Howett <duhowett@microsoft.com>
2017-05-30 11:00:25 -0700

    Add Tests.Helpers, a common repository for ObjC test helpers

---
+++ tests/frameworks/include/Helpers/TestHelpers.h
@@ -0,0 +1,34 @@
+//******************************************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
+
+#pragma once
+
+#import <Foundation/Foundation.h>
+
+@interface THRunLoopSpinner : NSObject
+@property (nonatomic, readonly) NSRunLoop* runLoop;
+- (void)scheduleAndAwaitBlock:(void (^)())block;
+@end
+
+// Convenience class that wraps an NSCondition and an associated boolean, and implements the NSCondition usage pattern documented in:
+// https://developer.apple.com/reference/foundation/nscondition?language=objc
+// This can be replaced/re-implemented based on NSConditionLock once that has a stable implementation
+@interface THBooleanCondition : NSObject
+- (BOOL)waitUntilDate:(NSDate*)limit;
+- (void)broadcast;
+@property (readonly) NSCondition* condition;
+@property (readonly) bool isOpen;
+@end
---
+++ tests/helpers/THBooleanCondition.mm
@@ -0,0 +1,49 @@
+//******************************************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
+
+#import <Helpers/TestHelpers.h>
+
+@implementation THBooleanCondition
+- (instancetype)init {
+    if (self = [super init]) {
+        _condition = [NSCondition new];
+        _isOpen = false;
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [_condition release];
+    [super dealloc];
+}
+
+- (BOOL)waitUntilDate:(NSDate*)limit {
+    BOOL ret = YES;
+    [_condition lock];
+    while (!_isOpen && ret) {
+        ret = [_condition waitUntilDate:limit];
+    }
+    [_condition unlock];
+    return ret;
+}
+
+- (void)broadcast {
+    [_condition lock];
+    _isOpen = YES;
+    [_condition broadcast];
+    [_condition unlock];
+}
+@end
