05b0a9a97706e85513b36dadb36fd5967ff6b3e8
David Chisnall <github@theravensnest.org>
2019-03-02 11:04:43 -0800

    Update NSConstantString for the GNUstep 2.0 ABI
    Signed-off-by: Dustin Howett <duhowett@microsoft.com>

--- Frameworks/Foundation/NSConstantString.h
+++ Frameworks/Foundation/NSConstantString.h
@@ -17,11 +17,43 @@
 #import <Foundation/NSString.h>
 #import "NSStringAdditionsForCF.h"
 
+#ifdef __OBJC_GNUSTEP_RUNTIME_ABI__
+#if __OBJC_GNUSTEP_RUNTIME_ABI__ >= 20
+#define WINOBJC_NEW_CONSTANT_STRINGS 1
+#endif
+#endif
+
 @interface NSConstantString : NSString <NSStringAdditionsForCF> {
-    // NOTE: these two ivars *must* be present to match the compiler expected
-    // layout for constant strings.
+    // NOTE: The ivar layout for this class *must* be present to match the
+    // compiler expected layout for constant strings.  This differs between the
+    // GCC-compatible ABI and the GNUstep v2 ABI.
+#ifdef WINOBJC_NEW_CONSTANT_STRINGS
+    /**
+     * Flags value.  The low 16 bits are reserved for the compiler, the top 16
+     * for the class implementation.  Currently, only the low 2 bits are
+     * defined by the compiler.
+     */
+    uint32_t flags;
+    /**
+     * The number of UTF-16 code units in the string.
+     */
+    uint32_t length;
+    /**
+     * The number of bytes occupied by the string.
+     */
+    uint32_t size;
+    /**
+     * Storage preallocated for the hash (not initialized by the compiler).
+     */
+    uint32_t hash;
+    /**
+     * Data pointer; format described by the bottom bits of `flags`.
+     */
+    const void* data;
+#else
     char* c_string;
     unsigned int len;
+#endif
 }
 
 - (instancetype)retain;
